6ade92c09da85ff8b7127870ffc4084224986271
Pat Marion <james.patrick.marion@gmail.com>
2013-10-31 15:24:14 -0400

    add switch for using app to support 3Dconnexion devices

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,19 @@
 #include <QApplication>
-#include <QVTKApplication.h>
-
 #include "ddMainWindow.h"
 #include "ddPythonManager.h"
 
+#define USE_TDX 0
+#if USE_TDX
+  #include <QVTKApplication.h>
+#endif
+
 int main(int argc, char **argv)
 {
+#if USE_TDX
   QVTKApplication app(argc, argv);
+#else
+  QApplication app(argc, argv);
+#endif
 
   ddPythonManager* pythonManager = new ddPythonManager;
 
