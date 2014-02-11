9795d3f2339d9f091cc1e47e47b5001d93e625ac
Matt DiCicco <mdicicco@drc008.(none)>
2014-02-11 17:53:36 -0500

    Adding helper returns when timeout is zero for reach plans as well.

--- src/python/ddapp/robotplanlistener.py
+++ src/python/ddapp/robotplanlistener.py
@@ -142,10 +142,6 @@ class ManipulationPlanDriver(object):
         self.sendPlannerSettings(startPose)
 
         if waitForResponse:
-            if waitTimeout == 0:
-                helper = lcmUtils.MessageResponseHelper(responseChannel, lcmdrc.robot_plan_w_keyframes_t)
-                lcmUtils.publish(requestChannel, msg)
-                return helper
             return lcmUtils.MessageResponseHelper.publishAndWait(requestChannel, msg,
                                     responseChannel, lcmdrc.robot_plan_w_keyframes_t, waitTimeout)
         else:
@@ -201,6 +197,10 @@ class ManipulationPlanDriver(object):
         self.sendPlannerSettings(startPose)
 
         if waitForResponse:
+            if waitTimeout == 0:
+                helper = lcmUtils.MessageResponseHelper(responseChannel, lcmdrc.robot_plan_w_keyframes_t)
+                lcmUtils.publish(requestChannel, msg)
+                return helper
             return lcmUtils.MessageResponseHelper.publishAndWait(requestChannel, msg,
                                     responseChannel, lcmdrc.robot_plan_w_keyframes_t, waitTimeout)
         else:
