a13dbcc663087ab9d1bfa85bb7ddeab144d7ab20
Pat Marion <james.patrick.marion@gmail.com>
2014-02-25 14:42:49 -0500

    add robotiq hand support to handdriver.py

--- src/python/ddapp/handdriver.py
+++ src/python/ddapp/handdriver.py
@@ -20,7 +20,7 @@ from ddapp import botpy
 
 import drc as lcmdrc
 import irobothand as lcmirobot
-
+import robotiqhand as lcmrobotiq
 
 
 
@@ -56,4 +56,46 @@ class IRobotHandDriver(object):
         msg.close_fraction = percentage / 100.0
 
         channel = 'IROBOT_%s_POSITION_CONTROL_CLOSE' % self.side.upper()
+        lcmUtils.publish(channel, msg)
+
+
+class RobotiqHandDriver(object):
+
+    def __init__(self, side):
+
+        assert side in ('left', 'right')
+        self.side = side
+        pass
+
+
+    def sendCalibrate(self):
+        msg = lcmrobotiq.command_t()
+        msg.utime = getUtime()
+        msg.activate = 0
+        msg.do_move = 0
+        msg.mode = 0
+        msg.position = 0
+        msg.force = 0
+        msg.velocity = 0
+        channel = 'ROBOTIQ_%s_COMMAND' % self.side.upper()
+        lcmUtils.publish(channel, msg)
+
+        time.sleep(0.1)
+        msg.activate = 1
+        lcmUtils.publish(channel, msg)
+
+    def sendOpen(self, percentage=100.0):
+        self.sendClose(100.0 - percentage)
+
+    def sendClose(self, percentage=100.0):
+        assert 0.0 <= percentage <= 100.0
+
+        msg = lcmrobotiq.command_t()
+        msg.utime = getUtime()
+        msg.activate = 1
+        msg.do_move = 1
+        msg.position = int(254 * (percentage/100.0))
+        msg.force = 255
+        msg.velocity = 254
+        channel = 'ROBOTIQ_%s_COMMAND' % self.side.upper()
         lcmUtils.publish(channel, msg)
