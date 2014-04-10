0e8bb8df8b8a41c01922f8a4a69ae9f80d8e161f
Maurice Fallon <mfallon@csail.mit.edu>
2014-04-09 22:02:22 -0400

    added all important plans - successfully walked over and back 4 times

--- src/python/ddapp/navigationpanel.py
+++ src/python/ddapp/navigationpanel.py
@@ -53,6 +53,7 @@ class NavigationPanel(object):
         self.ui.captureButton.connect("clicked()", self.onCaptureButton)
         self.ui.visualizeButton.connect("clicked()", self.onVisualizeButton)
         self.ui.planButton.connect("clicked()", self.onPlanButton)
+        self.ui.reversePlanButton.connect("clicked()", self.onReversePlanButton)
 
         self.goal = dict()
         
@@ -109,9 +110,25 @@ class NavigationPanel(object):
         request = self.footstepDriver.constructFootstepPlanRequest(startPose, goalFrame)
         self.footstepDriver.sendFootstepPlanRequest(request)
 
+    def onReversePlanButton(self):
+        print "reverse me"
+        
+        #self.clearFootstepPlan()
+        lastPlan = self.footstepDriver.lastFootstepPlan
+        reversedPlan = lastPlan.decode( lastPlan.encode() ) # decode and encode ensures deepcopy
+
+        #planFolder = getFootstepsFolder()
+        #self.drawFootstepPlan(msg, planFolder)
+
+        for i, footstep in enumerate(lastPlan.footsteps):
+          j = lastPlan.num_steps - 1 - i 
+          reversedPlan.footsteps[j] = footstep
+        
+        self.footstepDriver.onFootstepPlan(reversedPlan)
+        
     def pointPickerDemo(self,p1, p2):
-        print "mezo"
-        print p1, p2
+        #print "mezo"
+        #print p1, p2
         
         yaw = math.atan2( p2[1] - p1[1] , p2[0] - p1[0] )*180/math.pi + 90
         
@@ -127,26 +144,91 @@ class NavigationPanel(object):
         frame_pt_to_centerline.Concatenate(frame_p1)
          
         vis.updateFrame(frame_pt_to_centerline, "p1", parent="navigation")
-        
 
-        flist = np.array( [[ blockl*-0.5 , .1  , 0      , 0 , 0 , 0] ,
-                           [ blockl*-0.5 , -.1 , 0      , 0 , 0 , 0] ,
-	                   [ blockl*0.5 - 0.03 , .1  , blockh , 0 , 0 , 0] ,
-                           [ blockl*0.5 + 0.08  ,-.1  , blockh , 0 , 0 , 0] ,
-                           [ blockl*1.5        , .1  , 0      , 0 , 0 , 0] ,
-                           [ blockl*1.5 +0.03  ,-.1  , 0      , 0 , 0 , 0]])
-        #print flist
+# Original: 1 up/down
+#        flist = np.array( [[ blockl*-0.5 , .1  , 0      , 0 , 0 , 0] ,
+#                           [ blockl*-0.5 , -.1 , 0      , 0 , 0 , 0] ,
+#                           [ blockl*0.5 - 0.03 , .1  , blockh , 0 , 0 , 0] ,
+#                           [ blockl*0.5 + 0.06  ,-.1  , blockh , 0 , 0 , 0] ,
+#                           [ blockl*1.5        , .1  , 0      , 0 , 0 , 0] ,
+#                           [ blockl*1.5 +0.03  ,-.1  , 0      , 0 , 0 , 0]])
 
-        
+
+# Newer: 1 up/down
+#        flist = np.array( [[ blockl*-0.5       , .1  , 0      , 0 , 0 , 0] ,
+#                           [ blockl*-0.5       , -.1 , 0      , 0 , 0 , 0] ,
+#	                   [ blockl*0.5 - 0.03 , .1  , blockh , 0 , 0 , 0] ,
+#                           [ blockl*0.5 + 0.04 ,-.1  , blockh , 0 , 0 , 0] ,
+#                           [ blockl*1.5 - 0.03 , .1  , 0      , 0 , 0 , 0] ,
+#                           [ blockl*1.5 + 0.00 ,-.1  , 0      , 0 , 0 , 0]])
+
+# 3 up
+#        flist = np.array( [[ blockl*-0.5       , .1  , 0      , 0 , 0 , 0] ,
+#                           [ blockl*-0.5       , -.1 , 0      , 0 , 0 , 0] ,
+#	                   [ blockl*0.5 - 0.03 , .1  , blockh , 0 , 0 , 0] ,
+#                           [ blockl*0.5 + 0.0  ,-.1  , blockh , 0 , 0 , 0] ,
+#                           [ blockl*1.5 - 0.03 , .1  , 2*blockh, 0 , 0 , 0] ,
+#                           [ blockl*1.5 + 0.0  ,-.1  , 2*blockh, 0 , 0 , 0],
+#                           [ blockl*2.5 - 0.03 , .1  , 3*blockh, 0 , 0 , 0] ,
+#                           [ blockl*2.5 + 0.0  ,-.1  , 3*blockh, 0 , 0 , 0]])
+                           
+# 3 up and down (original)
+#        flist = np.array( [[ blockl*-0.5       , .1  , 0       , 0 , 0 , 0] ,
+#                           [ blockl*-0.5       , -.1 , 0       , 0 , 0 , 0] ,
+#	                   [ blockl*0.5 - 0.03 , .1  , blockh  , 0 , 0 , 0] ,
+#                           [ blockl*0.5 + 0.0  ,-.1  , blockh  , 0 , 0 , 0] ,
+#                           [ blockl*1.5 - 0.03 , .1  , 2*blockh, 0 , 0 , 0] ,
+#                           [ blockl*1.5 + 0.0  ,-.1  , 2*blockh, 0 , 0 , 0],
+#                           [ blockl*2.5 - 0.03 , .1  , 3*blockh, 0 , 0 , 0] ,
+#                           [ blockl*2.5 + 0.03  ,-.1 , 3*blockh, 0 , 0 , 0],
+#                           [ blockl*3.5 - 0.03 , .1  , 2*blockh, 0 , 0 , 0] ,
+#                           [ blockl*3.5 + 0.03  ,-.1 , 2*blockh, 0 , 0 , 0],
+#                           [ blockl*4.5 - 0.03 , .1  , 1*blockh, 0 , 0 , 0] ,
+#                           [ blockl*4.5 + 0.03  ,-.1 , 1*blockh, 0 , 0 , 0],
+#                           [ blockl*5.5 - 0.03 , .1  , 0       , 0 , 0 , 0] ,
+#                           [ blockl*5.5 + 0.03  ,-.1 , 0       , 0 , 0 , 0]])
+                           
+
+# 3 up and down
+        sep = 0.11
+        r =1
+        flist = np.array( [[ blockl*-0.5       , sep  , 0       , 0 , 0 , 0, 0],
+                           [ blockl*-0.5       , -sep , 0       , 0 , 0 , 0, r],
+	                   [ blockl*0.5 - 0.03 , sep  , blockh  , 0 , 0 , 0, 0],
+                           [ blockl*0.5 + 0.0  ,-sep  , blockh  , 0 , 0 , 0, r],
+                           [ blockl*1.5 - 0.03 , sep  , 2*blockh, 0 , 0 , 0, 0],
+                           [ blockl*1.5 + 0.0  ,-sep  , 2*blockh, 0 , 0 , 0, r],
+                           [ blockl*2.5 - 0.03 , sep  , 3*blockh, 0 , 0 , 0, 0],
+                           [ blockl*2.5 + 0.03  ,-sep , 3*blockh, 0 , 0 , 0, r],
+                           [ blockl*3.5 - 0.03 , sep  , 2*blockh, 0 , 0 , 0, 0],
+                           [ blockl*3.5 + 0.03  ,-sep , 2*blockh, 0 , 0 , 0, r],
+                           [ blockl*4.5 - 0.03 , sep  , 1*blockh, 0 , 0 , 0, 0],
+                           [ blockl*4.5 + 0.03  ,-sep , 1*blockh, 0 , 0 , 0, r],
+                           [ blockl*5.5 - 0.03 , sep  , 0       , 0 , 0 , 0, 0],
+                           [ blockl*5.5 + 0.03  ,-sep , 0       , 0 , 0 , 0, r], # half 
+                           [ blockl*5.5 - 0.03 , sep  , 0       , 0 , 0 , 0, 0], # extra step for planner
+                           [ blockl*4.5 + 0.03  ,-sep , 1*blockh, 0 , 0 , 0, r], # invert order
+                           [ blockl*4.5 - 0.03 , sep  , 1*blockh, 0 , 0 , 0, 0],
+                           [ blockl*3.5 + 0.03  ,-sep , 2*blockh, 0 , 0 , 0, r],
+                           [ blockl*3.5 - 0.03 , sep  , 2*blockh, 0 , 0 , 0, 0],
+                           [ blockl*2.5 + 0.03  ,-sep , 3*blockh, 0 , 0 , 0, r], # top
+                           [ blockl*2.5 - 0.06 , sep  , 3*blockh, 0 , 0 , 0, 0], # top
+                           [ blockl*1.5 + 0.04 ,-sep  , 2*blockh, 0 , 0 , 0, r],
+                           [ blockl*1.5 - 0.06 , sep  , 2*blockh, 0 , 0 , 0, 0],
+                           [ blockl*0.5 + 0.04 ,-sep  , blockh  , 0 , 0 , 0, r],
+	                   [ blockl*0.5 - 0.06 , sep  , blockh  , 0 , 0 , 0, 0],
+                           [ blockl*-0.5+ 0.04 , -sep , 0       , 0 , 0 , 0, r],
+                           [ blockl*-0.5 - 0.03, sep  , 0       , 0 , 0 , 0, 0]])
+                           
+                           
         contact_pts = self.footstepDriver.getContactPts()
         contact_pts_mid = np.mean(contact_pts, axis=0) # mid point on foot relative to foot frame
         foot_to_sole = transformUtils.frameFromPositionAndRPY( contact_pts_mid, [0,0,0]).GetLinearInverse()
         
-        
-        numGoalSteps = 6
-        is_right_foot = True
+        flist_shape = flist.shape
+        #is_right_foot = True
         self.goalSteps = []
-        for i in range(numGoalSteps):
+        for i in range(flist_shape[0]):
 
             #print flist[i,:]
             #print flist[i,0:3] 
@@ -159,10 +241,11 @@ class NavigationPanel(object):
             step_t.Concatenate(frame_pt_to_centerline)
 
  
-            is_right_foot = not is_right_foot
+            #is_right_foot = not is_right_foot
+            
             step = lcmdrc.footstep_t()
             step.pos = transformUtils.positionMessageFromFrame(step_t)
-            step.is_right_foot = is_right_foot
+            step.is_right_foot =  flist[i,6] # is_right_foot
             step.params = self.footstepDriver.getDefaultStepParams()
             
             vis.updateFrame(step_t, str(i), parent="navigation")
@@ -181,7 +264,7 @@ def init(jointController, footstepDriver, playbackRobotModel, playbackJointContr
 
     panel = NavigationPanel(jointController, footstepDriver, playbackRobotModel, playbackJointController)
     dock = app.addWidgetToDock(panel.widget)
-    dock.hide()
+    #dock.hide()
 
     return panel
 
