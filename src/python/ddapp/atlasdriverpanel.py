4822209be13bd225b4d988aa811b19ce3096b192
Pat Marion <james.patrick.marion@gmail.com>
2014-04-14 13:00:39 -0400

    combine atlas driver panel and atlas status panel. fixes #196

--- src/python/ddapp/atlasdriverpanel.py
+++ src/python/ddapp/atlasdriverpanel.py
@@ -1,21 +1,21 @@
 import PythonQt
-from PythonQt import QtCore, QtGui
-from ddapp import lcmUtils
+from PythonQt import QtCore, QtGui, QtUiTools
 from ddapp import applogic as app
-from ddapp.utime import getUtime
 from ddapp.timercallback import TimerCallback
 
-import numpy as np
-import math
+
+def addWidgetsToDict(widgets, d):
+
+    for widget in widgets:
+        if widget.objectName:
+            d[str(widget.objectName)] = widget
+        addWidgetsToDict(widget.children(), d)
 
 
-def _makeButton(text, func):
+class WidgetDict(object):
 
-    b = QtGui.QPushButton(text)
-    b.connect('clicked()', func)
-    return b
-
-
+    def __init__(self, widgets):
+        addWidgetsToDict(widgets, self.__dict__)
 
 
 
@@ -25,35 +25,60 @@ class AtlasDriverPanel(object):
 
         self.driver = driver
 
-        self.widget = QtGui.QWidget()
-        self.widget.setWindowTitle('Atlas Driver')
+        loader = QtUiTools.QUiLoader()
+        uifile = QtCore.QFile(':/ui/ddAtlasDriverPanel.ui')
+        assert uifile.open(uifile.ReadOnly)
 
-        l = QtGui.QVBoxLayout(self.widget)
+        self.widget = loader.load(uifile)
+        self.widget.setWindowTitle('Atlas Driver Panel')
+        self.ui = WidgetDict(self.widget.children())
 
-        l.addWidget(_makeButton('calibrate encoders', self.onCalibrateEncoders))
-        l.addWidget(_makeButton('calibrate bdi', self.onCalibrateBdi))
-        l.addWidget(_makeButton('prep', self.onPrep))
-        l.addWidget(_makeButton('user', self.onUser))
-        l.addWidget(QtGui.QLabel(''))
-        l.addWidget(_makeButton('stand', self.onStand))
-        l.addWidget(_makeButton('manip', self.onManip))
-        l.addWidget(QtGui.QLabel(''))
-        l.addWidget(_makeButton('freeze', self.onFreeze))
-        l.addWidget(_makeButton('stop', self.onStop))
-        l.addWidget(QtGui.QLabel(''))
+        self.ui.prepButton.connect('clicked()', self.onPrep)
+        self.ui.standButton.connect('clicked()', self.onStand)
 
-        self.behaviorLabel = QtGui.QLabel('current behavior: <unknown>')
-        l.addWidget(self.behaviorLabel)
-        l.addStretch()
+        self.ui.userButton.connect('clicked()', self.onUser)
+        self.ui.manipButton.connect('clicked()', self.onManip)
 
-        self.updateTimer = TimerCallback()
+        self.ui.stopButton.connect('clicked()', self.onStop)
+        self.ui.freezeButton.connect('clicked()', self.onFreeze)
+
+        self.ui.calibrateEncodersButton.connect('clicked()', self.onCalibrateEncoders)
+        self.ui.calibrateBdiButton.connect('clicked()', self.onCalibrateBdi)
+
+        PythonQt.dd.ddGroupBoxHider(self.ui.calibrationGroupBox)
+        PythonQt.dd.ddGroupBoxHider(self.ui.statusGroupBox)
+
+        self.updateTimer = TimerCallback(targetFps=5)
         self.updateTimer.callback = self.updatePanel
         self.updateTimer.start()
-
         self.updatePanel()
 
     def updatePanel(self):
-        self.behaviorLabel.text = 'current behavior: %s' % self.driver.getCurrentBehaviorName()
+        self.updateBehaviorLabel()
+        self.updateStatus()
+        self.updateButtons()
+
+    def updateBehaviorLabel(self):
+        self.ui.behaviorLabel.text = self.driver.getCurrentBehaviorName() or '<unknown>'
+
+    def updateStatus(self):
+        self.ui.inletPressure.value = self.driver.getCurrentInletPressure()
+        self.ui.supplyPressure.value = self.driver.getCurrentSupplyPressure()
+        self.ui.returnPressure.value = self.driver.getCurrentReturnPressure()
+        self.ui.airSumpPressure.value = self.driver.getCurrentAirSumpPressure()
+        self.ui.pumpRpm.value =  self.driver.getCurrentPumpRpm()
+
+    def updateButtons(self):
+
+        behavior = self.driver.getCurrentBehaviorName()
+        behaviorIsFreeze = behavior == 'freeze'
+
+        self.ui.calibrateBdiButton.setEnabled(behaviorIsFreeze)
+        self.ui.calibrateEncodersButton.setEnabled(behaviorIsFreeze)
+        self.ui.prepButton.setEnabled(behaviorIsFreeze)
+        self.ui.standButton.setEnabled(behavior in ('prep', 'stand', 'manip', 'step', 'walk'))
+        self.ui.manipButton.setEnabled(behavior in ('stand', 'manip'))
+        self.ui.userButton.setEnabled(behavior is not None)
 
     def onFreeze(self):
         self.driver.sendFreezeCommand()
@@ -81,13 +106,18 @@ class AtlasDriverPanel(object):
 
 
 
+def _getAction():
+    return app.getToolBarActions()['ActionAtlasDriverPanel']
+
+
 def init(driver):
 
     global panel
     global dock
 
     panel = AtlasDriverPanel(driver)
-    dock = app.addWidgetToDock(panel.widget)
+    dock = app.addWidgetToDock(panel.widget, action=_getAction())
     dock.hide()
+
 
     return panel
