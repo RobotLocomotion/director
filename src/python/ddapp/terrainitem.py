6c7b0171d3490683eee041fda0ca304f4d6127d0
Robin Deits <robin.deits@gmail.com>
2014-06-10 13:33:27 +0300

    use PlacerWidget to snap IRIS seed to terrain (when terrain available)

--- src/python/ddapp/terrainitem.py
+++ src/python/ddapp/terrainitem.py
@@ -7,24 +7,36 @@ import ddapp.visualization as vis
 from scipy.spatial.qhull import QhullError
 from ddapp import transformUtils
 from ddapp.debugVis import DebugData
+# from ddapp import vtkAll as vtk
+from ddapp.pointpicker import PlacerWidget
 from PythonQt import QtGui
 
 class TerrainRegionItem(vis.PolyDataItem):
     def __init__(self, name, view, seed_pose, terrain_segmentation):
 
         vis.PolyDataItem.__init__(self, name, None, view)
-
-        self.frameObj = vis.FrameItem('region seed', seed_pose, view)
-        om.addToObjectModel(self.frameObj, om.getOrCreateContainer('IRIS region seeds'))
-        self.frameObj.setProperty('Scale', 0.25)
-        self.frameObj.setProperty('Visible', True)
-        self.frameObj.setProperty('Edit', True)
+        self.transform = seed_pose
+        d = DebugData()
+        d.addSphere((0,0,0), radius=0.02)
+        self.seedObj = vis.showPolyData(d.getPolyData(), 'region seed', color=[.8,.8,.1], parent=om.getOrCreateContainer('IRIS region seeds'))
+        self.seedObj.actor.SetUserTransform(self.transform)
+        self.frameObj = vis.showFrame(self.transform, 'region seed frame',
+                                      scale=0.2,
+                                      visible=False,
+                                      parent=self.seedObj)
         self.frameObj.connectFrameModified(self.onFrameModified)
+        terrain = om.findObjectByName('HEIGHT_MAP_SCENE')
+        if terrain:
+            self.placer = PlacerWidget(view, self.seedObj, terrain)
+            self.placer.start()
+        else:
+            self.frameObj.setProperty('Edit', True)
+            self.frameObj.setProperty('Visible', True)
+
         self.terrain_segmentation = terrain_segmentation
         self.safe_region = None
         self.addProperty('Visible', True)
         self.addProperty('Enabled for Walking', True)
-        self.addProperty('Edit', True)
         self.addProperty('Color', QtGui.QColor(200,200,20))
         self.addProperty('Alpha', 1.0)
         self.onFrameModified(self.frameObj)
@@ -46,12 +58,10 @@ class TerrainRegionItem(vis.PolyDataItem):
                 else:
                     p2 = np.hstack((xy_verts[:,0], z))
                 debug.addLine(p1, p2, color=[.8,.8,.2])
-            debug.addSphere(pos, color=[.8, .8, .2])
             self.setPolyData(debug.getPolyData())
             self.safe_region = safe_region
         except QhullError:
             print "Could not generate convex hull (polytope is likely unbounded)."
-
 
     def _onPropertyChanged(self, propertySet, propertyName):
         vis.PolyDataItem._onPropertyChanged(self, propertySet, propertyName)
@@ -61,15 +71,10 @@ class TerrainRegionItem(vis.PolyDataItem):
                 self.setProperty('Alpha', 1.0)
             else:
                 self.setProperty('Alpha', 0.25)
-        elif propertyName in ['Visible', 'Edit']:
-            if self.getProperty('Visible') and self.getProperty('Edit'):
-                self.frameObj.setProperty('Visible', True)
-                self.frameObj.setProperty('Edit', True)
-            else:
-                self.frameObj.setProperty('Visible', False)
-                self.frameObj.setProperty('Edit', False)
-
+        if propertyName == 'Visible':
+            self.seedObj.setProperty('Visible', self.getProperty('Visible'))
 
     def onRemoveFromObjectModel(self):
         om.removeFromObjectModel(self.frameObj)
+        om.removeFromObjectModel(self.seedObj)
         vis.PolyDataItem.onRemoveFromObjectModel(self)
