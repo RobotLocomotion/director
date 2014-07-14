e9a4f42b5f24839ebaceb386351d298907e9f2e1
Pat Marion <james.patrick.marion@gmail.com>
2014-07-14 16:52:12 -0400

    add textured skybox visualization support

--- src/python/ddapp/startup.py
+++ src/python/ddapp/startup.py
@@ -26,6 +26,7 @@ from ddapp import objectmodel as om
 from ddapp import spreadsheet
 from ddapp import transformUtils
 from ddapp import tdx
+from ddapp import skybox
 from ddapp import perception
 from ddapp import segmentation
 from ddapp import cameraview
@@ -109,6 +110,7 @@ useFootContactVis = True
 useImageWidget = False
 useImageViewDemo = True
 useControllerRate = True
+useSkybox = False
 
 
 poseCollection = PythonQt.dd.ddSignalMap()
@@ -371,6 +373,17 @@ if useControllerRate:
             self.label.text = 'Controller rate: %.2f hz' % self.sub.getMessageRate()
 
     rateComputer = LCMMessageRateDisplay('ATLAS_COMMAND', 'Controller rate: %.2 hz', app.getMainWindow().statusBar())
+
+
+if useSkybox:
+
+    skyboxDataDir = os.path.expanduser('~/Downloads/skybox')
+    imageMap = skybox.getSkyboxImages(skyboxDataDir)
+    skyboxObjs = skybox.createSkybox(imageMap, view)
+    skybox.createTextureGround(os.path.join(skyboxDataDir, 'Dirt_seamless.jpg'), view)
+    skybox.connectSkyboxCamera(view)
+    view.camera().SetViewAngle(60)
+    om.findObjectByName('grid').setProperty('Visible', False)
 
 
 if useFootContactVis:
