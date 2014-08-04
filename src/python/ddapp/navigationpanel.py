804c3e15f2838b4187263551b00315a762e94a7a
Maurice Fallon <mfallon@mit.edu>
2014-08-04 16:02:24 -0400

    printf clean up

--- src/python/ddapp/navigationpanel.py
+++ src/python/ddapp/navigationpanel.py
@@ -72,29 +72,29 @@ class NavigationPanel(object):
 
         self.ui.startNewMapButton.connect("clicked()", self.onStartNewMapButton)
         self.ui.useNewMapButton.connect("clicked()", self.onUseNewMapButton)
-        
+
         # Data Variables:
         self.goal = dict()
         self.init_frame = None
         self.octomap_cloud = None
-        
+
     ###############################
-    
+
     def getSelectedGoalName(self):
         goal_name = self.ui.comboBox.currentText
         return goal_name
-    
+
     def getFrameFromCombo(self):
         pose = self.goal[ self.getSelectedGoalName() ]
         #frame = transformUtils.frameFromPositionAndRPY(pose[0:3], np.degrees(pose[3:6]) )
         return pose
-    
+
     def printTransform(self,t,message):
         p = t.GetPosition()
         q = t.GetOrientation()
         print "%f %f %f | %f %f %f | %s" % (p[0],p[1],p[2],q[0],q[1],q[2],message)
-    
-    
+
+
     def onCaptureButton(self):
         print "capture" #,self.jointController.q
         # body frame:
@@ -133,7 +133,7 @@ class NavigationPanel(object):
 
     def onReversePlanButton(self):
         print "reverse me"
-        
+
         #self.clearFootstepPlan()
         lastPlan = self.footstepDriver.lastFootstepPlan
         reversedPlan = lastPlan.decode( lastPlan.encode() ) # decode and encode ensures deepcopy
@@ -142,9 +142,9 @@ class NavigationPanel(object):
         #self.drawFootstepPlan(msg, planFolder)
 
         for i, footstep in enumerate(lastPlan.footsteps):
-          j = lastPlan.num_steps - 1 - i 
+          j = lastPlan.num_steps - 1 - i
           reversedPlan.footsteps[j] = footstep
-        
+
         self.footstepDriver.onFootstepPlan(reversedPlan)
 
     def onRestartNavButton(self):
@@ -160,25 +160,25 @@ class NavigationPanel(object):
         p1 = [0,0,0.85]
         init_frame = transformUtils.frameFromPositionAndRPY( p1 , [0,0,0] )
         vis.updateFrame(init_frame, "init pose", parent="navigation")
-        self.sendInitMessage(p1, 0)        
-        
-        
+        self.sendInitMessage(p1, 0)
+
+
     def pointPickerDemo(self,p1, p2):
         self.sendReadyMessage()
-      
+
         yaw = math.atan2( p2[1] - p1[1] , p2[0] - p1[0] )
-        p1[2] = p1[2] + 0.85       
+        p1[2] = p1[2] + 0.85
         init_frame = transformUtils.frameFromPositionAndRPY(p1, [0,0,yaw*180/math.pi])
         vis.updateFrame(init_frame, "init pose", parent="navigation")
-        self.sendInitMessage(p1, yaw)        
+        self.sendInitMessage(p1, yaw)
 
-        
+
     def sendReadyMessage(self):
         ready_init = lcmdrc.utime_t()
         ready_init.utime = getUtime()
         lcmUtils.publish('STATE_EST_READY', ready_init)
         sleep(1) # sleep needed to give SE time to restart
-        
+
 
     def sendInitMessage(self, pos, yaw):
         init = indexed_measurement_t()
@@ -187,25 +187,25 @@ class NavigationPanel(object):
         init.measured_dim = 4
         init.z_effective = [ pos[0], pos[1], pos[2] , yaw ]
         init.z_indices = [9, 10, 11, 8]
-        
+
         init.measured_cov_dim = init.measured_dim*init.measured_dim
         init.R_effective= [0] * init.measured_cov_dim
         init.R_effective[0]  = 0.25
         init.R_effective[5]  = 0.25
         init.R_effective[10] = 0.25
         init.R_effective[15] =  math.pow( 5*math.pi/180 , 2 )
-        
+
         lcmUtils.publish('MAV_STATE_EST_VIEWER_MEASUREMENT', init)
-        
-        
+
+
     def onShowMapButton(self):
         # reloads the map each time - in case its changed on disk:
         #if (self.octomap_cloud is None):
         filename = ddapp.getDRCBaseDir() + "/software/build/data/octomap.pcd"
         self.octomap_cloud = io.readPolyData(filename) # c++ object called vtkPolyData
-        
+
         assert (self.octomap_cloud.GetNumberOfPoints() !=0 )
-        
+
         # clip point cloud to height - doesnt work very well yet. need to know robot's height
         #self.octomap_cloud = segmentation.cropToLineSegment(self.octomap_cloud, np.array([0,0,-10]), np.array([0,0,3]) )
 
@@ -217,59 +217,59 @@ class NavigationPanel(object):
         folder = om.getOrCreateContainer("segmentation")
         om.removeFromObjectModel(folder)
         vis.showPolyData(self.octomap_cloud, 'prior map', alpha=1.0, color=[0,0,0.4])
-        
-        
+
+
     def onHideMapButton(self):
         folder = om.getOrCreateContainer("segmentation")
         om.removeFromObjectModel(folder)
 
-        
-    def onEnableLaserButton(self):   
+
+    def onEnableLaserButton(self):
         msg = lcmdrc.utime_t()
         msg.utime = getUtime()
         lcmUtils.publish('STATE_EST_LASER_ENABLE', msg)
-        
 
-    def onDisableLaserButton(self):   
+
+    def onDisableLaserButton(self):
         msg = lcmdrc.utime_t()
         msg.utime = getUtime()
         lcmUtils.publish('STATE_EST_LASER_DISABLE', msg)
-    
-    def onStartNewMapButton(self):   
+
+    def onStartNewMapButton(self):
         '''
         Send a trigger to the Create Octomap process to start a new map
         '''
         msg = lcmdrc.utime_t()
         msg.utime = getUtime()
-        lcmUtils.publish('STATE_EST_START_NEW_MAP', msg)    
+        lcmUtils.publish('STATE_EST_START_NEW_MAP', msg)
 
-    
-    def onUseNewMapButton(self): 
+
+    def onUseNewMapButton(self):
         '''
         Send a trigger to the GPF to use the newly created map
         '''
         msg = lcmdrc.utime_t()
         msg.utime = getUtime()
-        lcmUtils.publish('STATE_EST_USE_NEW_MAP', msg)    
-        
+        lcmUtils.publish('STATE_EST_USE_NEW_MAP', msg)
+
     def pointPickerDemoOriginal(self,p1, p2):
-        
+
         yaw = math.atan2( p2[1] - p1[1] , p2[0] - p1[0] )*180/math.pi + 90
         frame_p1 = transformUtils.frameFromPositionAndRPY(p1, [0,0,yaw])
-        
+
         blockl = 0.3937
         blockh = 0.142875
         sep = 0.11
-        
+
         frame_pt_to_centerline = transformUtils.frameFromPositionAndRPY( [0, -blockl/2, 0], [0,0,0])
-         
+
         frame_pt_to_centerline.PostMultiply()
         frame_pt_to_centerline.Concatenate(frame_p1)
-         
+
         vis.updateFrame(frame_pt_to_centerline, "corner", parent="navigation")
 
-        
-        
+
+
 # Original: 1 up/down
 #        flist = np.array( [[ blockl*-0.5 , .1  , 0      , 0 , 0 , 0] ,
 #                           [ blockl*-0.5 , -.1 , 0      , 0 , 0 , 0] ,
@@ -296,7 +296,7 @@ class NavigationPanel(object):
 #                           [ blockl*1.5 + 0.0  ,-.1  , 2*blockh, 0 , 0 , 0],
 #                           [ blockl*2.5 - 0.03 , .1  , 3*blockh, 0 , 0 , 0] ,
 #                           [ blockl*2.5 + 0.0  ,-.1  , 3*blockh, 0 , 0 , 0]])
-                           
+
 # 3 up and down (original)
 #        flist = np.array( [[ blockl*-0.5       , .1  , 0       , 0 , 0 , 0] ,
 #                           [ blockl*-0.5       , -.1 , 0       , 0 , 0 , 0] ,
@@ -312,7 +312,7 @@ class NavigationPanel(object):
 #                           [ blockl*4.5 + 0.03  ,-.1 , 1*blockh, 0 , 0 , 0],
 #                           [ blockl*5.5 - 0.03 , .1  , 0       , 0 , 0 , 0] ,
 #                           [ blockl*5.5 + 0.03  ,-.1 , 0       , 0 , 0 , 0]])
-                           
+
 
 # 3 up and down
         r =1
@@ -329,7 +329,7 @@ class NavigationPanel(object):
                            [ blockl*4.5 - 0.03 , sep  , 1*blockh, 0 , 0 , 0, 0],
                            [ blockl*4.5 + 0.03  ,-sep , 1*blockh, 0 , 0 , 0, r],
                            [ blockl*5.5 - 0.03 , sep  , 0       , 0 , 0 , 0, 0],
-                           [ blockl*5.5 + 0.03  ,-sep , 0       , 0 , 0 , 0, r], # half 
+                           [ blockl*5.5 + 0.03  ,-sep , 0       , 0 , 0 , 0, r], # half
                            [ blockl*5.5 - 0.03 , sep  , 0       , 0 , 0 , 0, 0], # extra step for planner
                            [ blockl*4.5 + 0.03  ,-sep , 1*blockh, 0 , 0 , 0, r], # invert order
                            [ blockl*4.5 - 0.03 , sep  , 1*blockh, 0 , 0 , 0, 0],
@@ -343,8 +343,8 @@ class NavigationPanel(object):
 	                   [ blockl*0.5 - 0.06 , sep  , blockh  , 0 , 0 , 0, 0],
                            [ blockl*-0.5+ 0.04 , -sep , 0       , 0 , 0 , 0, r],
                            [ blockl*-0.5 - 0.03, sep  , 0       , 0 , 0 , 0, 0]])
-                           
-                           
+
+
 # 2 up and down
         r =1
         flist = np.array( [[ blockl*-0.5       , sep  , 0       , 0 , 0 , 0, 0],
@@ -356,7 +356,7 @@ class NavigationPanel(object):
                            [ blockl*2.5 - 0.03 , sep  , 1*blockh, 0 , 0 , 0, 0],
                            [ blockl*2.5 + 0.03  ,-sep , 1*blockh, 0 , 0 , 0, r], # 1d
                            [ blockl*3.5 - 0.03 , sep  , 0       , 0 , 0 , 0, 0],
-                           [ blockl*3.5 + 0.03  ,-sep , 0       , 0 , 0 , 0, r], # half 
+                           [ blockl*3.5 + 0.03  ,-sep , 0       , 0 , 0 , 0, r], # half
                            [ blockl*3.5 - 0.03 , sep  , 0       , 0 , 0 , 0, 0], # extra step for planner
                            [ blockl*2.5 + 0.03  ,-sep , 1*blockh, 0 , 0 , 0, r], # invert order
                            [ blockl*2.5 - 0.03 , sep  , 1*blockh, 0 , 0 , 0, 0],
@@ -366,13 +366,13 @@ class NavigationPanel(object):
 	                   [ blockl*0.5 - 0.06 , sep  , blockh  , 0 , 0 , 0, 0],
                            [ blockl*-0.5+ 0.04 , -sep , 0       , 0 , 0 , 0, r],
                            [ blockl*-0.5 - 0.03, sep  , 0       , 0 , 0 , 0, 0]])
-                                                      
-                           
-                           
+
+
+
         contact_pts = self.footstepDriver.getContactPts()
         contact_pts_mid = np.mean(contact_pts, axis=0) # mid point on foot relative to foot frame
         foot_to_sole = transformUtils.frameFromPositionAndRPY( contact_pts_mid, [0,0,0]).GetLinearInverse()
-        
+
         flist_shape = flist.shape
         self.goalSteps = []
         for i in range(flist_shape[0]):
@@ -393,7 +393,7 @@ class NavigationPanel(object):
         request = self.footstepDriver.constructFootstepPlanRequest(startPose)
         request.num_goal_steps = len(self.goalSteps)
         request.goal_steps = self.goalSteps
-        lcmUtils.publish('FOOTSTEP_PLAN_REQUEST', request)        
+        lcmUtils.publish('FOOTSTEP_PLAN_REQUEST', request)
 
 
 def _getAction():
