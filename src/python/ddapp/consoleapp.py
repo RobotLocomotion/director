51284a6531653aa2be1866b94d41edcca7a08e9d
Pat Marion <james.patrick.marion@gmail.com>
2014-10-27 10:37:34 -0400

    use objectmodel.Icons.getIcon in consoleapp.py

--- src/python/ddapp/consoleapp.py
+++ src/python/ddapp/consoleapp.py
@@ -72,7 +72,7 @@ class ConsoleApp(object):
         applogic.addShortcut(view, 'F8', self.showPythonConsole)
         applogic.addShortcut(view, 'F9', self.showObjectModel)
 
-        view.setWindowIcon(om.Icons.Robot)
+        view.setWindowIcon(om.Icons.getIcon(om.Icons.Robot))
         view.setWindowTitle('View')
 
         return view
