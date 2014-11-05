ede3f5ee346125d69c49a66ff2f66ed878f65c79
Robin Deits <robin.deits@gmail.com>
2014-11-05 17:07:37 -0500

    add a new interface for quickly finding c-space regions w/o IRIS
    This just uses a simple local linearization to convert a convex polygon
    in 2D into a convex region in x,y,yaw configuration space

--- src/python/ddapp/terrain.py
+++ src/python/ddapp/terrain.py
@@ -7,9 +7,12 @@ from scipy.spatial import ConvexHull
 from scipy.io import loadmat, savemat
 
 import drc
+import ddapp.transformUtils
 from irispy.cspace import rotmat, cspace3
 from irispy.iris import inflate_region
-from irispy.utils import lcon_to_vert
+from irispy.utils import lcon_to_vert, sample_convex_polytope
+import polyhedron._cdd
+from polyhedron import Vrep, Hrep
 from py_drake_utils.utils import rpy2rotmat
 
 DEFAULT_FOOT_CONTACTS = np.array([[-0.13, -0.13, 0.17, 0.17],
@@ -134,6 +137,72 @@ class PolygonSegmentation(IRISInterface):
         A_bounds = np.vstack((-np.eye(3), np.eye(3)))
         b_bounds = np.hstack((-lb, ub))
         return A_bounds, b_bounds
+
+
+class PolygonSegmentationNonIRIS(IRISInterface):
+    def __init__(self, polygon_vertices, bot_pts=DEFAULT_FOOT_CONTACTS,
+                 bounding_box_width=DEFAULT_BOUNDING_BOX_WIDTH):
+
+        polygon_vertices = np.asarray(polygon_vertices)
+        self.planar_polyhedron = Vrep(polygon_vertices[:2,:].T)
+        self.bot_pts = bot_pts
+
+    def findSafeRegion(self, pose):
+        pose = np.asarray(pose)
+        t = ddapp.transformUtils.frameFromPositionAndRPY([0,0,0], pose[3:] * 180 / np.pi)
+
+        contact_pts_on_plane = np.zeros((2, self.bot_pts.shape[1]))
+        for j in range(self.bot_pts.shape[1]):
+            contact_pts_on_plane[:,j] = t.TransformPoint([self.bot_pts[0,j], self.bot_pts[1,j], 0])[:2]
+
+        Rdot = np.array([[0, -1], [1, 0]])
+        contact_vel_in_world = Rdot.dot(contact_pts_on_plane)
+
+        c_region = {'A': [], 'b': []}
+
+        for i in range(self.planar_polyhedron.A.shape[0]):
+            ai = self.planar_polyhedron.A[i,:]
+            n = np.linalg.norm(ai)
+            ai = ai / n
+            bi = self.planar_polyhedron.b[i] / n
+
+            p = ai.dot(contact_pts_on_plane)
+            v = ai.dot(contact_vel_in_world)
+
+            mask = np.logical_or(p >= 0, v >= 0)
+            for j, tf in enumerate(mask):
+                if tf:
+                    c_region['A'].append(np.hstack((ai, v[j])))
+                    c_region['b'].append([bi - p[j]])
+
+        A = np.vstack(c_region['A'])
+        b = np.hstack(c_region['b'])
+        self.c_space_polyhedron = Vrep(Hrep(A, b).generators)
+
+        return SafeTerrainRegion(self.c_space_polyhedron.A,
+                                 self.c_space_polyhedron.b,
+                                 [], [], pose)
+
+    def drawSamples(self, nsamples):
+        import matplotlib.pyplot as plt
+        plt.figure(1)
+        plt.clf()
+        plt.hold(True)
+        k = ConvexHull(self.bot_pts.T).vertices
+        k = np.hstack((k, k[0]))
+        n = self.planar_polyhedron.generators.shape[0]
+        plt.plot(self.planar_polyhedron.generators.T[0,range(n) + [0]],
+                 self.planar_polyhedron.generators.T[1,range(n) + [0]], 'r.-')
+        samples = sample_convex_polytope(self.c_space_polyhedron.A,
+                                         self.c_space_polyhedron.b,
+                                         500)
+        for i in range(samples.shape[1]):
+            R = np.array([[np.cos(samples[2,i]), -np.sin(samples[2,i])],
+                          [np.sin(samples[2,i]), np.cos(samples[2,i])]])
+            V = R.dot(self.bot_pts[:,k])
+            V = V + samples[:2, i].reshape((2,1))
+            plt.plot(V[0,:], V[1,:], 'k-')
+        plt.show()
 
 
 class TerrainSegmentation(IRISInterface):
