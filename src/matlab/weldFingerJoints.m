b82068067feb5d9e6aeaf14df36da095d66cc97d
Andrés Valenzuela <avalenzu@mit.edu>
2015-01-21 09:39:19 -0500

    Fix weldFingerJoints whitespace

--- src/matlab/weldFingerJoints.m
+++ src/matlab/weldFingerJoints.m
@@ -1,33 +1,33 @@
 function robot = weldFingerJoints(robot)
 
-    finger_joints = {...
-        'right_f0_j0'
-        'right_f0_j1'
-        'right_f0_j2'
-        'right_f1_j0'
-        'right_f1_j1'
-        'right_f1_j2'
-        'right_f2_j0'
-        'right_f2_j1'
-        'right_f2_j2'
-        'right_f3_j0'
-        'right_f3_j1'
-        'right_f3_j2'
-        'left_f0_j0'
-        'left_f0_j1'
-        'left_f0_j2'
-        'left_f1_j0'
-        'left_f1_j1'
-        'left_f1_j2'
-        'left_f2_j0'
-        'left_f2_j1'
-        'left_f2_j2'
-        'left_f3_j0'
-        'left_f3_j1'
-        'left_f3_j2'}';
+  finger_joints = {...
+    'right_f0_j0'
+    'right_f0_j1'
+    'right_f0_j2'
+    'right_f1_j0'
+    'right_f1_j1'
+    'right_f1_j2'
+    'right_f2_j0'
+    'right_f2_j1'
+    'right_f2_j2'
+    'right_f3_j0'
+    'right_f3_j1'
+    'right_f3_j2'
+    'left_f0_j0'
+    'left_f0_j1'
+    'left_f0_j2'
+    'left_f1_j0'
+    'left_f1_j1'
+    'left_f1_j2'
+    'left_f2_j0'
+    'left_f2_j1'
+    'left_f2_j2'
+    'left_f3_j0'
+    'left_f3_j1'
+    'left_f3_j2'}';
 
-    for joint_name = finger_joints
-      robot = robot.weldJoint(joint_name{:}, 1);
-    end
+  for joint_name = finger_joints
+    robot = robot.weldJoint(joint_name{:}, 1);
+  end
 
 end
