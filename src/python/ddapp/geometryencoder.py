17e53a9d3991742a320eaecca75a5cbb838dbfb5
Pat Marion <james.patrick.marion@gmail.com>
2015-02-02 23:33:23 -0500

    add code to geometryencoder.py to support mac homebrew vtk
    homebrew's vtk doesn't have the parallel kit, so it doesn't have the
    vtkCommunicator class.

--- src/python/ddapp/geometryencoder.py
+++ src/python/ddapp/geometryencoder.py
@@ -1,5 +1,6 @@
 from ddapp import vtkAll as vtk
 from ddapp import vtkNumpy as vnp
+from ddapp.shallowCopy import shallowCopy
 import numpy as np
 
 def encodePolyData(polyData):
@@ -7,6 +8,13 @@ def encodePolyData(polyData):
     the serialization of the data.  This array can be passed to the
     decodePolyData function to construct a new vtkPolyData object from
     the serialized data.'''
+
+    if not hasattr(vtk, 'vtkCommunicator'):
+        w = vtk.vtkPolyDataWriter()
+        w.WriteToOutputStringOn()
+        w.SetInput(polyData)
+        w.Write()
+        return np.frombuffer(w.GetOutputStdString(), dtype=np.int8)
 
     charArray = vtk.vtkCharArray()
     vtk.vtkCommunicator.MarshalDataObject(polyData, charArray)
@@ -17,6 +25,13 @@ def encodePolyData(polyData):
 def decodePolyData(data):
     '''Given a numpy int8 array, deserializes the data to construct a new
     vtkPolyData object and returns the result.'''
+
+    if not hasattr(vtk, 'vtkCommunicator'):
+        r = vtk.vtkPolyDataReader()
+        r.ReadFromInputStringOn()
+        r.SetInputString(str(data.data))
+        r.Update()
+        return shallowCopy(r.GetOutput())
 
     charArray = vnp.getVtkFromNumpy(data)
     assert isinstance(charArray, vtk.vtkCharArray)
