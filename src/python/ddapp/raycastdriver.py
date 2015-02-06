8aacec56c0b8ea3fdfdee53a934ec4ae40858215
Robin Deits <robin.deits@gmail.com>
2015-02-06 16:46:47 -0500

    center raycast terrain on robot

--- src/python/ddapp/raycastdriver.py
+++ src/python/ddapp/raycastdriver.py
@@ -7,18 +7,18 @@ class RaycastDriver(object):
     def __init__(self, request_channel='TERRAIN_RAYCAST_REQUEST'):
         self.request_channel = request_channel
 
-    def requestRaycast(self, affordances):
+    def requestRaycast(self, affordances, lb=[-2,-2], ub=[2,2], step=0.02):
         urdfStr = affordanceurdf.urdfStringFromAffordances(affordances)
         msg = lcmdrc.terrain_raycast_request_t()
         msg.urdf = lcmdrc.robot_urdf_t()
         msg.urdf.robot_name = "collision_environment"
         msg.urdf.urdf_xml_string = urdfStr
-        msg.x_min = -2
-        msg.x_step = 0.02
-        msg.x_max = 2
-        msg.y_min = -2
-        msg.y_step = 0.02
-        msg.y_max = 2
+        msg.x_min = lb[0]
+        msg.x_step = step
+        msg.x_max = ub[0]
+        msg.y_min = lb[1]
+        msg.y_step = step
+        msg.y_max = ub[1]
 
         max_height = None
         for aff in affordances:
