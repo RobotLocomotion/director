a582ed7f37f6c38b93016e37f9200250f7888f94
Pat Marion <james.patrick.marion@gmail.com>
2015-03-06 15:11:33 -0500

    fix compile errors in ddDrakeModel.
    robot model visualization is currently broken:
    todo: need to load visual information
    todo: need to map jointname to dof index for kinematics and joint limits

---
+++ src/python/tests/testLoadUrdf.py
@@ -0,0 +1,12 @@
+from ddapp.consoleapp import ConsoleApp
+from ddapp import visualization as vis
+from ddapp import roboturdf
+
+app = ConsoleApp()
+view = app.createView()
+
+robotStateModel, robotStateJointController = roboturdf.loadRobotModel('robot state model', view, parent='sensors', color=roboturdf.getRobotGrayColor(), visible=True)
+
+if app.getTestingInteractiveEnabled():
+    view.show()
+    app.start()
