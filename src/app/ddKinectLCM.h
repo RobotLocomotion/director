54ff8b6bf99009c467a0b973b8ec577acf4b2a6b
cdarpino <cdarpino@mit.edu>
2015-03-18 00:18:09 -0400

    removing unused code

--- src/app/ddKinectLCM.h
+++ src/app/ddKinectLCM.h
@@ -2,7 +2,6 @@
 #define __ddKinectLCM_h
 
 #include <QObject>
-#include <ddMacros.h>
 
 #include "ddLCMThread.h"
 #include "ddLCMSubscriber.h"
@@ -10,21 +9,10 @@
 #include <string>
 #include <sstream>
 
-#include <Eigen/Geometry>
 #include <lcm/lcm-cpp.hpp>
-#include <lcmtypes/bot_core/image_t.hpp>
-#include <lcmtypes/bot_core/images_t.hpp>
-
-#include <bot_core/camtrans.h>
-#include <bot_param/param_client.h>
-#include <bot_param/param_util.h>
 #include <bot_frames/bot_frames.h>
 
-#include <image_utils/jpeg.h>
-
 #include <vtkSmartPointer.h>
-#include <vtkPNGWriter.h>
-#include <vtkImageData.h>
 #include <vtkPolyData.h>
 #include <vtkPointData.h>
 #include <vtkUnsignedCharArray.h>
@@ -35,68 +23,24 @@
 #include <kinect/kinect-utils.h>
 
 class ddKinectLCM : public QObject
- {
+{
   Q_OBJECT
 
 public:
 
-  class CameraData
-  {
-    public:
-
-    bool mHasCalibration;
-    std::string mName;
-    std::string mCoordFrame;
-    BotCamTrans* mCamTrans;
-    bot_core::image_t mImageMessage;
-    Eigen::Isometry3d mLocalToCamera;
-    Eigen::Isometry3d mBodyToCamera;
-    std::vector<uint8_t> mImageBuffer;
-    QMutex mMutex;
-
-    CameraData()
-    {
-      mHasCalibration = false;
-      mImageMessage.width = 0;
-      mImageMessage.height = 0;
-      mImageMessage.utime = 0;
-    }
-
-    ~CameraData()
-    {
-      if (mCamTrans) bot_camtrans_destroy(mCamTrans);
-    }
-  };
-
   ddKinectLCM(QObject* parent=NULL);
-
-  virtual ~ddKinectLCM();
   
   void init(ddLCMThread* lcmThread, const QString& botConfigFile);
   void getPointCloudFromKinect(vtkPolyData* polyDataRender);
 
 protected slots:
 
-  // void onImagesMessage(const QByteArray& data, const QString& channel);
-  // void onImageMessage(const QByteArray& data, const QString& channel);
   void onKinectFrame(const QByteArray& data, const QString& channel);
 
 
 protected:
 
-  // CameraData* getCameraData(const QString& cameraName);
-  // bool initCameraData(const QString& cameraName, CameraData* cameraData);
-
-  vtkSmartPointer<vtkImageData> toVtkImage(CameraData* cameraData);
-
-  BotParam* mBotParam;
-  BotFrames* mBotFrames;
-
   ddLCMThread* mLCM;
-  QMap<QString, QMap<int, QString> > mChannelMap;
-  QMap<QString, bot_core::images_t> mImagesMessageMap;
-  QMap<QString, ddLCMSubscriber*> mSubscribers;
-  QMap<QString, CameraData*> mCameraData;
 
   KinectCalibration* kcal;
   int decimate_;
