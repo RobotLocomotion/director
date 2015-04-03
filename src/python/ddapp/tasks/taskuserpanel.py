60bf58d5e1cc2b6eade6092520c52bda0ae8ddd0
Pat Marion <james.patrick.marion@gmail.com>
2015-04-03 18:39:50 -0400

    add task user panel base class to generalize valve task panel

---
+++ src/python/ddapp/tasks/taskuserpanel.py
@@ -0,0 +1,278 @@
+import os
+import sys
+import vtkAll as vtk
+from ddapp import botpy
+import math
+import time
+import types
+import functools
+import numpy as np
+
+from ddapp import transformUtils
+from ddapp import lcmUtils
+from ddapp.timercallback import TimerCallback
+from ddapp.asynctaskqueue import AsyncTaskQueue
+from ddapp import objectmodel as om
+from ddapp import visualization as vis
+from ddapp import applogic as app
+from ddapp.debugVis import DebugData
+from ddapp import ik
+from ddapp import ikplanner
+from ddapp import ioUtils
+from ddapp.simpletimer import SimpleTimer
+from ddapp.utime import getUtime
+from ddapp.pointpicker import ImagePointPicker
+from ddapp import affordanceitems
+from ddapp import affordanceupdater
+from ddapp import robotstate
+from ddapp import robotplanlistener
+from ddapp import cameraview
+from ddapp import segmentation
+from ddapp import planplayback
+from ddapp import propertyset
+from ddapp import asynctaskqueue as atq
+
+import ddapp.tasks.robottasks as rt
+import ddapp.tasks.taskmanagerwidget as tmw
+
+import PythonQt
+from PythonQt import QtCore, QtGui, QtUiTools
+
+
+import drc as lcmdrc
+import traceback
+from PythonQt import QtCore, QtGui
+
+
+
+
+def addWidgetsToDict(widgets, d):
+
+    for widget in widgets:
+        if widget.objectName:
+            d[str(widget.objectName)] = widget
+        addWidgetsToDict(widget.children(), d)
+
+
+class WidgetDict(object):
+
+    def __init__(self, widgets):
+        addWidgetsToDict(widgets, self.__dict__)
+
+
+class TaskUserPanel(object):
+
+    def __init__(self):
+
+
+        loader = QtUiTools.QUiLoader()
+        uifile = QtCore.QFile(':/ui/ddTaskUserPanel.ui')
+        assert uifile.open(uifile.ReadOnly)
+
+        self.widget = loader.load(uifile)
+        self.ui = WidgetDict(self.widget.children())
+
+        self.manualButtons = {}
+        self.imageViewLayout = QtGui.QVBoxLayout(self.ui.imageFrame)
+
+        self._setupParams()
+        self._setupPropertiesPanel()
+        self._initTaskPanel()
+
+
+    def addManualButton(self, text, callback):
+        b = QtGui.QPushButton(text)
+        b.connect('clicked()', callback)
+        self.manualButtons[text] = b
+        self.addManualWidget(b)
+        return b
+
+    def addManualSpacer(self):
+
+        line = QtGui.QFrame()
+        line.setFrameShape(QtGui.QFrame.HLine)
+        line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.addManualWidget(line)
+
+    def addManualWidget(self, widget):
+        self.ui.manualButtonsLayout.insertWidget(self.ui.manualButtonsLayout.count()-1, widget)
+
+    def initImageView(self, imageView):
+        self.affordanceUpdater = affordanceupdater.AffordanceInCameraUpdater(segmentation.affordanceManager, imageView)
+        self.affordanceUpdater.timer.start()
+        self.imageViewLayout.addWidget(self.fitter.imageView.view)
+
+
+    def _setupParams(self):
+        self.params = om.ObjectModelItem('Task Params')
+        self.params.properties.connectPropertyChanged(self.onPropertyChanged)
+
+
+    def _setupPropertiesPanel(self):
+        l = QtGui.QVBoxLayout(self.ui.propertyFrame)
+        l.setMargin(0)
+        self.propertiesPanel = PythonQt.dd.ddPropertiesPanel()
+        self.propertiesPanel.setBrowserModeToWidget()
+        l.addWidget(self.propertiesPanel)
+        self.panelConnector = propertyset.PropertyPanelConnector(self.params.properties, self.propertiesPanel)
+
+
+    def onPropertyChanged(self, propertySet, propertyName):
+        pass
+
+
+    def getNextTasks(self):
+        return self.taskTree.getTasks(fromSelected=True)
+
+    def onContinue(self):
+
+        self.completedTasks = []
+        self.taskQueue.reset()
+        for obj in self.getNextTasks():
+            self.taskQueue.addTask(obj.task)
+
+        self.taskQueue.start()
+
+
+    def onStep(self):
+
+        assert not self.taskQueue.isRunning
+
+        tasks = self.getNextTasks()
+        if not tasks:
+            return
+
+        task = tasks[0].task
+        self.nextStepTask = tasks[1].task if len(tasks) > 1 else None
+
+        self.completedTasks = []
+        self.taskQueue.reset()
+        self.taskQueue.addTask(task)
+        self.taskQueue.start()
+
+
+    def onPause(self):
+
+        if not self.taskQueue.isRunning:
+            return
+
+        self.nextStepTask = None
+        currentTask = self.taskQueue.currentTask
+        self.taskQueue.stop()
+        if currentTask:
+            currentTask.stop()
+
+        self.appendMessage('<font color="red">paused</font>')
+
+
+    def onTaskStarted(self, taskQueue, task):
+        msg = task.properties.getProperty('Name')  + ' ... <font color="green">start</font>'
+        self.appendMessage(msg)
+
+        self.taskTree.selectTask(task)
+        item = self.taskTree.findTaskItem(task)
+        if len(self.completedTasks) and item.getProperty('Visible'):
+            self.appendMessage('<font color="red">paused</font>')
+            raise atq.AsyncTaskQueue.PauseException()
+
+    def onTaskEnded(self, taskQueue, task):
+        msg = task.properties.getProperty('Name') + ' ... <font color="green">end</font>'
+        self.appendMessage(msg)
+
+        self.completedTasks.append(task)
+
+        if self.taskQueue.tasks:
+            self.taskTree.selectTask(self.taskQueue.tasks[0])
+        elif self.nextStepTask:
+            self.taskTree.selectTask(self.nextStepTask)
+        #else:
+        #    self.taskTree.selectTask(self.completedTasks[0])
+
+    def onTaskFailed(self, taskQueue, task):
+        msg = task.properties.getProperty('Name')  + ' ... <font color="red">failed: %s</font>' % task.failReason
+        self.appendMessage(msg)
+
+    def onTaskPaused(self, taskQueue, task):
+        msg = task.properties.getProperty('Name')  + ' ... <font color="red">paused</font>'
+        self.appendMessage(msg)
+
+    def onTaskException(self, taskQueue, task):
+        msg = task.properties.getProperty('Name')  + ' ... <font color="red">exception:\n\n%s</font>' % traceback.format_exc()
+        self.appendMessage(msg)
+
+
+    def appendMessage(self, msg):
+        if msg == self.lastStatusMessage:
+            return
+
+        self.lastStatusMessage = msg
+        self.ui.outputConsole.append(msg.replace('\n', '<br/>'))
+        #print msg
+
+    def updateTaskStatus(self):
+
+        currentTask = self.taskQueue.currentTask
+        if not currentTask or not currentTask.statusMessage:
+            return
+
+        name = currentTask.properties.getProperty('Name')
+        status = currentTask.statusMessage
+        msg = name + ': ' + status
+        self.appendMessage(msg)
+
+    def onAcceptPrompt(self):
+        self.promptTask.accept()
+        self.promptTask = None
+        self.ui.promptLabel.text = ''
+        self.ui.promptAcceptButton.enabled = False
+        self.ui.promptRejectButton.enabled = False
+
+    def onRejectPrompt(self):
+
+        self.promptTask.reject()
+        self.promptTask = None
+        self.ui.promptLabel.text = ''
+        self.ui.promptAcceptButton.enabled = False
+        self.ui.promptRejectButton.enabled = False
+
+    def onTaskPrompt(self, task, message):
+        self.promptTask = task
+        self.ui.promptLabel.text = message
+        self.ui.promptAcceptButton.enabled = True
+        self.ui.promptRejectButton.enabled = True
+
+    def _initTaskPanel(self):
+
+        self.lastStatusMessage = ''
+        self.nextStepTask = None
+        self.completedTasks = []
+        self.taskQueue = atq.AsyncTaskQueue()
+        self.taskQueue.connectTaskStarted(self.onTaskStarted)
+        self.taskQueue.connectTaskEnded(self.onTaskEnded)
+        self.taskQueue.connectTaskPaused(self.onTaskPaused)
+        self.taskQueue.connectTaskFailed(self.onTaskFailed)
+        self.taskQueue.connectTaskException(self.onTaskException)
+
+        self.timer = TimerCallback(targetFps=2)
+        self.timer.callback = self.updateTaskStatus
+        self.timer.start()
+
+        rt.UserPromptTask.promptFunction = self.onTaskPrompt
+        rt.PrintTask.printFunction = self.appendMessage
+
+        self.taskTree = tmw.TaskTree()
+        self.ui.taskFrame.layout().insertWidget(0, self.taskTree.treeWidget)
+
+        l = QtGui.QVBoxLayout(self.ui.taskPropertiesGroupBox)
+        l.addWidget(self.taskTree.propertiesPanel)
+        PythonQt.dd.ddGroupBoxHider(self.ui.taskPropertiesGroupBox)
+
+
+        self.ui.taskStepButton.connect('clicked()', self.onStep)
+        self.ui.taskContinueButton.connect('clicked()', self.onContinue)
+        self.ui.taskPauseButton.connect('clicked()', self.onPause)
+
+        self.ui.promptAcceptButton.connect('clicked()', self.onAcceptPrompt)
+        self.ui.promptRejectButton.connect('clicked()', self.onRejectPrompt)
+        self.ui.promptAcceptButton.enabled = False
+        self.ui.promptRejectButton.enabled = False
