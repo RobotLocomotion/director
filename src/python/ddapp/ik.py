6cac0df84b1f4800655e41bf4739a105cb204e0a
Pat Marion <james.patrick.marion@gmail.com>
2015-05-03 21:57:43 -0400

    update ikserver start logic to better handle restarts
    now the ikserver will not re-issue startup commands if the external matlab
    process is already initialized

--- src/python/ddapp/ik.py
+++ src/python/ddapp/ik.py
@@ -20,6 +20,7 @@ class AsyncIKCommunicator():
         self.comm = None
         self.outputConsole = None
         self.ready = False
+        self.restarted = False
         self.robotURDF = robotURDF
         self.fixedPointFile = fixedPointFile
 
@@ -50,7 +51,10 @@ class AsyncIKCommunicator():
         self.callbacks = callbacks.CallbackRegistry([self.STARTUP_COMPLETED])
 
 
-    def getStartupCommands(self):
+    def _sendStartupCommands(self):
+
+        if self.restarted:
+            return
 
         commands = []
         commands.append('\n%-------- startup --------\n')
@@ -61,7 +65,7 @@ class AsyncIKCommunicator():
         commands.append("fixed_point_file = [getenv('DRC_BASE'), '/%s'];" % os.path.relpath(self.fixedPointFile, ddapp.getDRCBaseDir()))
         commands.append('runIKServer')
         commands.append('\n%------ startup end ------\n')
-        return commands
+        return self.comm.sendCommandsAsync(commands)
 
     def _createMatlabClient(self):
 
@@ -79,8 +83,10 @@ class AsyncIKCommunicator():
 
         taskQueue = AsyncTaskQueue()
         taskQueue.addTask(functools.partial(self.comm.sendCommandsAsync, ['\n']))
-        taskQueue.addTask(functools.partial(self.comm.sendCommandsAsync, self.getStartupCommands()))
+        taskQueue.addTask(self._checkServerRestarted)
+        taskQueue.addTask(self._sendStartupCommands)
         taskQueue.addTask(self._checkServerStartup)
+        taskQueue.addTask(self._notifyStartupCompleted)
         taskQueue.addTask(functools.partial(setattr, self.comm, 'echoToStdOut', True))
 
         self.taskQueue = taskQueue
@@ -93,11 +99,12 @@ class AsyncIKCommunicator():
         self.callbacks.disconnect(callbackId)
 
     def _checkServerStartup(self):
-        try:
-            self.ready = self.comm.getFloatArray('ikServerStarted')[0] == 1
-        except:
-            self.ready = False
-        self._notifyStartupCompleted()
+        started = self.comm.getFloatArray("exist('ikServerStarted')")
+        self.ready = len(started) and started[0] == 1
+
+    def _checkServerRestarted(self):
+        self._checkServerStartup()
+        self.restarted = self.ready
 
     def _notifyStartupCompleted(self):
         self.callbacks.process(self.STARTUP_COMPLETED, self, self.ready)
