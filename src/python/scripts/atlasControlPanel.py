b421bea3cdfbe64a0e6093530a1ea0d3f2f9a8e7
Robin Deits <robin.deits@gmail.com>
2015-05-06 12:02:46 -0400

    easier-to-read buttons

--- src/python/scripts/atlasControlPanel.py
+++ src/python/scripts/atlasControlPanel.py
@@ -9,15 +9,23 @@ atlasDriver = atlasdriver.init()
 w = QtGui.QWidget()
 l = QtGui.QVBoxLayout(w)
 
-Button = namedtuple('Button', ['name', 'callback']);
-buttons = [Button('Freeze', atlasDriver.sendFreezeCommand),
-           Button('Stop', atlasDriver.sendStopCommand),
-           Button('Reactive Recovery', atlasDriver.sendRecoveryTriggerOn)]
+Button = namedtuple('Button', ['name', 'callback', 'color']);
+buttons = [
+           Button('Stop', atlasDriver.sendStopCommand, "#D11919"),
+           Button('Freeze', atlasDriver.sendFreezeCommand, None),
+           Button('Reactive Recovery', atlasDriver.sendRecoveryTriggerOn, None)
+           ]
 
 for button in buttons:
     qb = QtGui.QPushButton(button.name)
     qb.connect('clicked()', button.callback)
     qb.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+
+    s = qb.styleSheet
+    s += "font: 36pt;"
+    if button.color:
+        s += "background-color: {:s}; color: white;".format(button.color)
+    qb.setStyleSheet(s)
     l.addWidget(qb)
 
 w.setWindowTitle('Atlas Control Panel')
