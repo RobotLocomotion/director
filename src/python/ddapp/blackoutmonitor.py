441be20eaaf8e155e3d31cadabb5577b5b65bc52
Gregory Izatt <gizatt@mit.edu>
2015-05-20 13:37:49 -0400

    Add blackout length est

--- src/python/ddapp/blackoutmonitor.py
+++ src/python/ddapp/blackoutmonitor.py
@@ -14,26 +14,34 @@ from time import sleep
 
 class BlackoutMonitor(object):
     UPDATE_RATE = 5
+    AVERAGE_N = 10
     def __init__(self, robotStateJointController, view, cameraview):
 
         self.robotStateJointController = robotStateJointController
         self.view = view
         self.cameraview = cameraview
 
-        self.last_message_time = 0
+        self.lastMessageTime = 0
+
+        self.lastBlackoutLengths = []
+        self.lastBlackoutLength = 0
+        self.inBlackout = False
+        self.averageBlackoutLength = 30.0
 
         self.updateTimer = TimerCallback(self.UPDATE_RATE)
         self.updateTimer.callback = self.update
         self.updateTimer.start()
 
     def update(self):
-        self.last_message_time = self.cameraview.imageManager.queue.getCurrentImageTime('CAMERA_LEFT')
+        #self.lastMessageTime = self.cameraview.imageManager.queue.getCurrentImageTime('CAMERA_LEFT')
         if self.robotStateJointController.lastRobotStateMessage:
-            elapsed = (self.robotStateJointController.lastRobotStateMessage.utime - self.last_message_time) / (1000*1000)
+            elapsed = (self.robotStateJointController.lastRobotStateMessage.utime - self.lastMessageTime) / (1000*1000)
             if elapsed > 1.0:
+                self.inBlackout = True
+                self.lastBlackoutLength = elapsed
                 # blackout!
                 ssize = self.view.size
-                textstr = "BLACKOUT: " + str(elapsed) + " sec"
+                textstr = "BLACKOUT: %3.2f / %3.2f sec" % (elapsed, self.averageBlackoutLength)
                 txt = vis.updateText(textstr, "blackout text", view=self.view)
                 txt.setProperty('Font Size', 24)
                 txt.setProperty('Position', (25, ssize.height()-50))
@@ -44,3 +52,11 @@ class BlackoutMonitor(object):
                 txt = om.findObjectByName('blackout text')
                 if (txt):
                     txt.setProperty('Visible', False)
+                if (self.inBlackout):
+                    if (self.lastBlackoutLength < 100000):
+                        self.lastBlackoutLengths.append(self.lastBlackoutLength)
+                    if len(self.lastBlackoutLengths) > self.AVERAGE_N:
+                        self.lastBlackoutLengths.pop(0)
+                    if len(self.lastBlackoutLengths) > 0:
+                        self.averageBlackoutLength = sum(self.lastBlackoutLengths) / float(len(self.lastBlackoutLengths))
+                    self.inBlackout = False
