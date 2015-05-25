20d8a820a3625efb991369b791292a5f906665e9
Wolfgang Merkt <w.merkt@gmail.com>
2015-05-25 18:51:19 +0100

    KinectLCM: settings panel similar to MultiSense

--- src/python/ddapp/kinectlcm.py
+++ src/python/ddapp/kinectlcm.py
@@ -23,73 +23,152 @@ import multisense as lcmmultisense
 from ddapp.consoleapp import ConsoleApp
 
 
-def init():
-    global KinectQueue
-    KinectQueue = PythonQt.dd.ddKinectLCM(lcmUtils.getGlobalLCMThread())
-    KinectQueue.init(lcmUtils.getGlobalLCMThread(), drcargs.args().config_file)
-    # global app, view
-    # app = ConsoleApp()
-    # app.setupGlobals(globals())
-    # app.showPythonConsole()
-    # view = app.createView()
-    # view.show()
-    # app.start()
+class KinectItem(om.ObjectModelItem):
+
+    def __init__(self, model):
+
+        om.ObjectModelItem.__init__(self, 'Kinect', om.Icons.Eye)
+
+        self.model = model
+        self.scalarBarWidget = None
+        self.addProperty('Color By', 1,
+                         attributes=om.PropertyAttributes(enumNames=['Solid Color', 'rgb_colors']))
+        self.addProperty('Updates Enabled', True)
+        self.addProperty('Framerate', model.targetFps,
+                         attributes=om.PropertyAttributes(decimals=0, minimum=1.0, maximum=30.0, singleStep=1, hidden=False))
+        self.addProperty('Visible', model.visible)
+        #self.addProperty('Point Size', model.pointSize,
+        #                 attributes=om.PropertyAttributes(decimals=0, minimum=1, maximum=20, singleStep=1, hidden=False))
+        #self.addProperty('Alpha', model.alpha,
+        #                 attributes=om.PropertyAttributes(decimals=2, minimum=0, maximum=1.0, singleStep=0.1, hidden=False))
+
+        #self.addProperty('Color', QtGui.QColor(255,255,255))
+        
+    def _onPropertyChanged(self, propertySet, propertyName):
+        om.ObjectModelItem._onPropertyChanged(self, propertySet, propertyName)
+
+        if propertyName == 'Updates Enabled':
+            if self.getProperty('Updates Enabled'):
+                self.model.start()
+            else:
+                self.model.stop()
+
+        #elif propertyName == 'Alpha':
+        #    self.model.setAlpha(self.getProperty(propertyName))
+
+        elif propertyName == 'Visible':
+            self.model.setVisible(self.getProperty(propertyName))
+
+        #elif propertyName == 'Point Size':
+        #    self.model.setPointSize(self.getProperty(propertyName))
+
+        elif propertyName == 'Framerate':
+            self.model.setFPS(self.getProperty('Framerate'))
+
+        elif propertyName == 'Color By':
+            self._updateColorBy()
+
+        self.model.polyDataObj._renderAllViews()
 
 
-def renderLastKinectPointCloud():
-    # view = view or app.getCurrentRenderView()
-    # if view is None:
-    #     return
-    p = vtk.vtkPolyData()
-    print("will grab the last point cloud in python \n")
-    KinectQueue.getPointCloudFromKinect(p)
-    print("grabbed the last point cloud in python, will render now \n")
-    obj = vis.showPolyData (p, 'kinect cloud')
-    print("director rendered last point cloud \n")
+    def _updateColorBy(self):
+
+        arrayMap = {
+          0 : 'Solid Color',
+          1 : 'rgb_colors'
+          }
+
+        colorBy = self.getProperty('Color By')
+        arrayName = arrayMap.get(colorBy)
+
+        self.model.polyDataObj.setProperty('Color By', arrayName)
 
 
-def startKinectLCM(targetFps=30):
 
-    global source, obj, t, _targetFps
-    _targetFps = targetFps
-    p = vtk.vtkPolyData()
+class KinectSource(TimerCallback):
 
-    utime = KinectQueue.getPointCloudFromKinect(p)
-    obj = vis.showPolyData(shallowCopy(p), 'kinect source')
+    def __init__(self, view, _KinectQueue):
+        self.view = view
+        self.KinectQueue = _KinectQueue
 
-    queue = PythonQt.dd.ddBotImageQueue(lcmUtils.getGlobalLCMThread())
-    queue.init(lcmUtils.getGlobalLCMThread(), drcargs.args().config_file)
+        self.visible = True
+        
+        self.p = vtk.vtkPolyData()
+        utime = KinectQueue.getPointCloudFromKinect(self.p)
+        self.polyDataObj = vis.PolyDataItem('kinect source', shallowCopy(self.p), view)
+        self.polyDataObj.actor.SetPickable(1)
+        self.polyDataObj.initialized = False
 
-    print obj.getArrayNames()
-    obj.initialized = False
+        self.queue = PythonQt.dd.ddBotImageQueue(lcmUtils.getGlobalLCMThread())
+        self.queue.init(lcmUtils.getGlobalLCMThread(), drcargs.args().config_file)
 
-    def updateSource():
+        self.targetFps = 30
+        self.timerCallback = TimerCallback(targetFps=self.targetFps)
+        self.timerCallback.callback = self._updateSource
+        #self.timerCallback.start()
+        
+    def start(self):
+        self.timerCallback.start()
 
+    def stop(self):
+        self.timerCallback.stop()
+
+    def setFPS(self, framerate):
+        self.targetFps = framerate
+        self.timerCallback.stop()
+        self.timerCallback.targetFps = framerate
+        self.timerCallback.start()
+
+    def setVisible(self, visible):
+        self.polyDataObj.setProperty('Visible', visible)
+
+    def _updateSource(self):
         p = vtk.vtkPolyData()
-        utime = KinectQueue.getPointCloudFromKinect(p)
+        utime = self.KinectQueue.getPointCloudFromKinect(p)
 
         if not p.GetNumberOfPoints():
             return
 
         cameraToLocalFused = vtk.vtkTransform()
-        queue.getTransform('KINECT_RGB', 'local', utime, cameraToLocalFused)
-        #vis.updateFrame( cameraToLocalFused , 'cameraToLocalFused', visible=True, scale = 0.2)
-        #print utime , cameraToLocalFused.GetPosition()
+        self.queue.getTransform('KINECT_RGB', 'local', utime, cameraToLocalFused)
         p = filterUtils.transformPolyData(p,cameraToLocalFused)
-        obj.setPolyData(p)
+        self.polyDataObj.setPolyData(p)
 
-        if not obj.initialized:
-            obj.setProperty('Color By', 'rgb_colors')
-            obj.initialized = True
+        if not self.polyDataObj.initialized:
+            self.polyDataObj.setProperty('Color By', 'rgb_colors')
+            self.polyDataObj.initialized = True
 
 
-    global timerCallback
-    timerCallback = TimerCallback(targetFps=_targetFps)
-    timerCallback.callback = updateSource
-    timerCallback.start()
+def init(view):
+    global KinectQueue, _kinectItem, _kinectSource
+    KinectQueue = PythonQt.dd.ddKinectLCM(lcmUtils.getGlobalLCMThread())
+    KinectQueue.init(lcmUtils.getGlobalLCMThread(), drcargs.args().config_file)
+    
+    _kinectSource = KinectSource(view, KinectQueue)
+    _kinectSource.start()
+
+    sensorsFolder = om.getOrCreateContainer('sensors')
+
+    _kinectItem = KinectItem(_kinectSource)
+    om.addToObjectModel(_kinectItem, sensorsFolder)
+    
+
+# Hasn't been used - currently deactivated
+#def renderLastKinectPointCloud():
+#    # view = view or app.getCurrentRenderView()
+#    # if view is None:
+#    #     return
+#    p = vtk.vtkPolyData()
+#    print("will grab the last point cloud in python \n")
+#    KinectQueue.getPointCloudFromKinect(p)
+#    print("grabbed the last point cloud in python, will #render now \n")
+#    obj = vis.showPolyData (p, 'kinect cloud')
+#    print("director rendered last point cloud \n")
+
 
 def startButton():
-    init()
-    startKinectLCM()
+    view = app.getCurrentRenderView()
+    init(view)
+    _kinectSource.start()
 
 app.addToolbarMacro('start live kinect', startButton)
