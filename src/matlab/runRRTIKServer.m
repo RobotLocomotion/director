c1fdf2b98361c9dfc2c3074dbd690eda1d8721ed
marcoCar <marcocaravagna@gmail.com>
2015-07-22 16:42:18 +0100

    small mods to visualize normals and textures correctly

---
+++ src/matlab/runRRTIKServer.m
@@ -0,0 +1,38 @@
+s = IKServer();
+
+warning('off','Drake:RigidBodyManipulator:UnsupportedVelocityLimits');
+warning('off','Drake:RigidBodyManipulator:UnsupportedContactPoints');
+warning('off','Drake:RigidBodyManipulator:UnsupportedJointLimits');
+
+s.robot = r;
+s.plan_publisher = RobotPlanPublisherWKeyFrames('CANDIDATE_MANIP_PLAN', true, s.robot.getPositionFrame.coordinates);
+s = s.compile();
+s = s.setupCosts();
+s = s.loadNominalData(fixed_point_file);
+
+r = s.robot_and_environment;
+
+nq = r.getNumPositions();
+q_nom = s.q_nom;
+q_zero = zeros(nq, 1);
+q_end = q_nom;
+
+try
+  l_foot_pts = s.getLinkPoints(left_foot_link);
+  r_foot_pts = s.getLinkPoints(right_foot_link);
+  pelvis_pts = s.getPelvisPoints();
+catch
+  % no-op
+end
+
+links = struct();
+linknames = s.getLinkNames();
+for i = 1:size(linknames, 1)
+  nameComponents = regexp(linknames{i}, '\+', 'split');
+  for name = nameComponents
+    links.(name{1}) = i;
+  end
+end
+
+joints = Point(r.getStateFrame, (1:r.getStateFrame.dim)');
+ikServerStarted = true;
