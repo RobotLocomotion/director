19eb90adb0507c559a38f6b6fbbe0fff2d4279bd
marcoCar <marcocaravagna@gmail.com>
2015-08-24 14:07:57 +0100

    clean multiRRT_reach script

--- src/matlab/test_multiRRT_reach.m
+++ src/matlab/test_multiRRT_reach.m
@@ -8,14 +8,10 @@ function info = test_multiRRT_reach(r, s, grHand, qStart, xGoal, objectGrasped)
   point_in_link_frame.left = [0.08; 0.07; 0];
   point_in_link_frame = point_in_link_frame.(grHand);
   
-%   r.body(hand.(grHand)).collision_geometry  
-%   r.body(hand.(grHand)).visual_geometry
-  
   addpath(fullfile(getDrakePath(), '..', 'ddapp/src/matlab') )
   fixed_point_file = [getDrakePath(), '/../control/matlab/data/val_description/valkyrie_fp_june2015.mat'];
-  left_foot_link = 'LeftFoot';
-  right_foot_link = 'RightFoot';
-%   runRRTIKServer
+  capability_map_file = [getDrakePath(), '/../control/matlab/data/val_description/capabilityMap.mat'];
+  
   kinsol = r.doKinematics(qStart);
   xStart = [r.forwardKin(kinsol, hand.(grHand), point_in_link_frame, 2); qStart'];
   
@@ -27,9 +23,7 @@ function info = test_multiRRT_reach(r, s, grHand, qStart, xGoal, objectGrasped)
   rightFootPose = r.forwardKin(kinsol,r_foot, [0; 0; 0], 2);
   rightFootPosConstraint = WorldPositionConstraint(r, r_foot, [0; 0; 0], rightFootPose(1:3), rightFootPose(1:3));
   rightFootQuatConstraint = WorldQuatConstraint(r, r_foot, rightFootPose(4:7), 0.0, [0.0, 1.0]);
-  
-%   xGoal = [(leftFootPose(1:2) + rightFootPose(1:2))/2; 0]' + [0.1; -0.3; 0.9]'; 
-  
+    
   xGoal = xGoal(:);
   goalFrame = [eye(3) xGoal(1:3); 0 0 0 1];
   goalEulerConstraint = WorldEulerConstraint(r, hand.(grHand), [0;0; -pi], [0; 0; pi]);
@@ -55,9 +49,9 @@ function info = test_multiRRT_reach(r, s, grHand, qStart, xGoal, objectGrasped)
   nonGraspingHandConstraint = Point2PointDistanceConstraint(r, elbow.(grHand), trunk, [0; 0; 0], [0; 0; 0], 0.2, Inf);
       
   additionalConstraints = {leftFootPosConstraint, leftFootQuatConstraint,...
-    rightFootPosConstraint, rightFootQuatConstraint, quasiStaticConstraint};%, nonGraspingHandConstraint};
+    rightFootPosConstraint, rightFootQuatConstraint, quasiStaticConstraint, nonGraspingHandConstraint};
   
-  cm = CapabilityMap([fileparts(which('exploringRRT')) '/CapabilityMap/capabilityMap.mat']);
+  cm = CapabilityMap(capability_map_file);
   
   LeftHipYawLink = r.findLinkId('LeftHipYawLink');
   RightHipYawLink = r.findLinkId('RightHipYawLink');
@@ -105,21 +99,19 @@ function info = test_multiRRT_reach(r, s, grHand, qStart, xGoal, objectGrasped)
   options.terrain = RigidBodyFlatTerrain(); %Changed to a smaller terrain to avoid visualization problem when zooming
   options.joint_v_max = 15*pi/180;
   
-  fp = load([getDrakePath(), '/../control/matlab/data/val_description/valkyrie_fp_june2015.mat']);
+  fp = load(fixed_point_file);
   qNom = fp.xstar(1:r.getNumPositions());
-  xGoal
   multiTree = MultipleTreeProblem(r, hand.(grHand), xStart, xGoal, [],...
     goalConstraints, additionalConstraints, qNom,...
     'capabilityMap', cm, 'graspingHand', grHand, 'activecollisionoptions',...
     struct('body_idx', setdiff(1:r.getNumBodies(), inactive_collision_bodies)),...
     'ikoptions', ikoptions, 'endeffectorpoint', point_in_link_frame);
   [~, info, ~, q_path] = multiTree.rrt(options);
-  disp(info)
-  info = info.status;
-  disp(info)
   
-  path_length = size(q_path,2);  
-  q_traj = PPTrajectory(pchip(linspace(0, 1, path_length), q_path(8:end,:)));
-  s.publishTraj(q_traj, 1);  
+  if info == 1
+    path_length = size(q_path,2);  
+    q_traj = PPTrajectory(pchip(linspace(0, 1, path_length), q_path(8:end,:)));
+    s.publishTraj(q_traj, 1);
+  end
   
 end
