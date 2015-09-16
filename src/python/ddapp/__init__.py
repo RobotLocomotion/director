2bbd3cbbd4c06268b75263aca5056e9022c91bce
Pat Marion <james.patrick.marion@gmail.com>
2015-09-16 18:48:31 -0400

    add additional sys paths in ddapp/__init__.py

--- src/python/ddapp/__init__.py
+++ src/python/ddapp/__init__.py
@@ -26,3 +26,10 @@ _initCoverage()
 
 # this is for mac homebrew users
 updateSysPath('/usr/local/opt/vtk5/lib/python2.7/site-packages')
+
+# this inserts the build dirs in the path before anything else
+# this ensures modules in the build dirs will take precedence over
+# system versions (homebrew)
+for relDir in ['../../dist-packages', '../../site-packages']:
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), relDir)))
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), relDir)))
