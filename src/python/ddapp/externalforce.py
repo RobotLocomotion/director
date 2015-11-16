847ce64c5ab257449505214bb90387e6379b3ede
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-11-15 19:48:39 -0500

    updates for parsing particles stored in files, and plotting of PCF estimates

--- src/python/ddapp/externalforce.py
+++ src/python/ddapp/externalforce.py
@@ -26,6 +26,7 @@ from ddapp.timercallback import TimerCallback
 
 
 import os
+import csv
 import functools
 import numpy as np
 import scipy.io
@@ -44,22 +45,34 @@ class ExternalForce(object):
         self.externalForces = dict()
         self.timeout = 1e10 # this is for testing
         self.publishChannel = 'EXTERNAL_FORCE_TORQUE'
+        self.captureMode = False
+        self.captureModeCounter = 0
+        self.addSubscribers()
 
         # setup timercallback to publish, lets say at 5 hz
         self.timer = TimerCallback(targetFps=5)
         self.timer.callback = self.publish
         self.startPublishing()
 
+    def addSubscribers(self):
+        lcmUtils.addSubscriber('CONTACT_FILTER_POINT_ESTIMATE', lcmdrc.contact_filter_estimate_t, self.onContactEstimate)
+
 
     # linkName is a string, wrench is an np.array
     def addForce(self, linkName, wrench=None, forceDirection=None, forceMagnitude=None, forceLocation=None, inWorldFrame=False):
 
-
+        linkName = str(linkName) # getting a weird u in front otherwise
         d = dict()
         # need at least one of wrench, or forceDirection and forceMagnitude
         assert (wrench is not None) or ((forceDirection is not None) and (forceMagnitude is not None) and (forceLocation is not None))
 
-        visName = linkName + ' external force'
+        if self.captureMode:
+            self.captureModeCounter += 1
+            key = linkName + "_" + str(self.captureModeCounter)
+        else:
+            key = linkName
+
+        visName = key + ' external force'
         om.removeFromObjectModel(om.findObjectByName(visName))
 
 
@@ -71,6 +84,7 @@ class ExternalForce(object):
             d['forceDirection'] = wrench[3:]/np.linalg.norm(wrench[3:])
             d['forceMagnitude'] = np.linalg.norm(wrench[3:])
             d['isWrench'] = True
+            d['linkName'] = linkName
         else:
             if inWorldFrame:
                 linkToWorld = self.robotStateModel.getLinkFrame(linkName)
@@ -83,11 +97,15 @@ class ExternalForce(object):
             d['forceMagnitude'] = forceMagnitude
             d['forceLocation'] = forceLocation
             d['isWrench'] = False
+            d['linkName'] = linkName
+
+
+        
 
 
         d['time'] = time.time()
-        self.externalForces[linkName] = d
-        self.updateContactWrench(linkName)
+        self.externalForces[key] = d
+        self.updateContactWrench(key)
         self.drawForces()
 
     def computeWrench(self, linkName, forceDirection, forceMagnitude, forceLocation):
@@ -104,21 +122,32 @@ class ExternalForce(object):
         return wrenchTransformed
 
 
-    def removeForce(self, linkName, callFromFrameObj=False):
-        self.externalForces.pop(linkName, None)
-        name = linkName + ' external force'
+    def removeForce(self, key, callFromFrameObj=False):
+        if not self.externalForces.has_key(key):
+            return
+
+        visObjectName = key + ' external force'
+        self.externalForces.pop(key, None)        
 
         if not callFromFrameObj:
-            om.removeFromObjectModel(om.findObjectByName(linkName))
+            om.removeFromObjectModel(om.findObjectByName(visObjectName))
+
+    def removeAllForces(self):
+        keyList = list(self.externalForces.keys())
+
+        for key in keyList:
+            self.removeForce(key)
+
+
 
     # remove forces from dict that haven't been refreshed in at least self.timeout seconds
     def removeStaleExternalForces(self):
         keysToRemove = []
-        for linkName, value in self.externalForces.iteritems():
+        for key, value in self.externalForces.iteritems():
             elapsed = time.time() - value['time']
     
             if elapsed > self.timeout:
-                keysToRemove.append(linkName)
+                keysToRemove.append(key)
 
 
         for key in keysToRemove:
@@ -136,8 +165,8 @@ class ExternalForce(object):
         msg.num_external_forces = len(self.externalForces);
 
 
-        for linkName, val in self.externalForces.iteritems():
-            msg.body_names.append(linkName)
+        for key, val in self.externalForces.iteritems():
+            msg.body_names.append(val['linkName'])
             msg.tx.append(val['wrench'][0])
             msg.ty.append(val['wrench'][1])
             msg.tz.append(val['wrench'][2])
@@ -149,10 +178,53 @@ class ExternalForce(object):
 
 
     def startPublishing(self):
+        self.captureMode = False
+        self.captureModeCounter = 0
+        self.removeAllForces()
         self.timer.start()
 
     def stopPublishing(self):
+        print "stopping publishing"
         self.timer.stop()
+
+    def startCaptureMode(self):
+        self.stopPublishing()
+        print "starting capture mode"
+        self.removeAllForces()
+        self.captureMode = True
+        self.captureModeCounter = 0
+
+    def onContactEstimate(self, msg):
+        name = 'estimated external force'
+        forceLocation = np.array(msg.contact_position)
+        force = np.array(msg.contact_force)
+
+        eps = 0.5
+        if np.linalg.norm(force) < eps:
+            om.removeFromObjectModel(om.findObjectByName(name))
+            return
+
+        self.drawForce(name, msg.body_name, forceLocation, force, color=[0,0,1])
+
+
+    def drawForce(self, name, linkName, forceLocation, force, color):
+        forceDirection = force/np.linalg.norm(force)
+        om.removeFromObjectModel(om.findObjectByName(name))
+
+        linkToWorld = self.robotStateModel.getLinkFrame(linkName)
+        forceLocationInWorld = np.array(linkToWorld.TransformPoint(forceLocation))
+        forceDirectionInWorld = np.array(linkToWorld.TransformDoubleVector(forceDirection))
+
+        point = forceLocationInWorld - 0.1*forceDirectionInWorld
+
+        d = DebugData()
+        # d.addArrow(point, forceLocationInWorld, headRadius=0.025, tubeRadius=0.005, color=color)
+        d.addSphere(forceLocationInWorld, radius=0.01)
+        d.addLine(point, forceLocationInWorld, radius=0.005)
+
+        obj = vis.updatePolyData(d.getPolyData(), name, color=color)
+        obj.actor.SetPickable(False)
+
 
     # connect this with an on model changed
     def drawForces(self):
@@ -161,8 +233,9 @@ class ExternalForce(object):
 
 
 
-        for linkName, val in self.externalForces.iteritems():
-            name = linkName + ' external force'
+        for key, val in self.externalForces.iteritems():
+            linkName = val['linkName']
+            name = key + ' external force'
             linkToWorld = self.robotStateModel.getLinkFrame(linkName)
 
             forceLocationInWorld = np.array(linkToWorld.TransformPoint(val['forceLocation']))
@@ -185,6 +258,7 @@ class ExternalForce(object):
             obj.connectRemovedFromObjectModel(self.removeForceFromFrameObject)
             obj.addProperty('magnitude', 0.0)
             obj.addProperty('linkName', linkName)
+            obj.addProperty('key', key)
             obj.properties.connectPropertyChanged(functools.partial(self.onPropertyChanged, obj))
 
     def onModelChanged(self, model):
@@ -193,6 +267,7 @@ class ExternalForce(object):
     def onPropertyChanged(self, frameObj, propertySet, propertyName):
         if propertyName != 'magnitude':
             return
+        key = frameObj.getProperty('key')
         linkName = frameObj.getProperty('linkName')
         magnitude = frameObj.getProperty('magnitude')
         if magnitude < 0:
@@ -200,29 +275,74 @@ class ExternalForce(object):
             print "external forces can only PUSH, NOT PULL"
             return
 
-        self.externalForces[linkName]['forceMagnitude'] = magnitude
-        self.updateContactWrench(linkName)
+        self.externalForces[key]['forceMagnitude'] = magnitude
+        self.updateContactWrench(key)
 
-    def updateContactWrench(self, linkName):
-        if not self.externalForces.has_key(linkName):
+    def updateContactWrench(self, key):
+        if not self.externalForces.has_key(key):
             return
 
-        val = self.externalForces[linkName]
+        val = self.externalForces[key]
 
         # if it was specified as a wrench, then don't overwrite it
         if val['isWrench']:
             return
 
-        val['wrench'] = self.computeWrench(linkName, val['forceDirection'],  val['forceMagnitude'], val['forceLocation'])
+        val['wrench'] = self.computeWrench(val['linkName'], val['forceDirection'],  val['forceMagnitude'], val['forceLocation'])
 
 
     def removeForceFromFrameObject(self, tree_, frameObj):
-        linkName = frameObj.getProperty('linkName')
-        self.removeForce(linkName, callFromFrameObj=True)
+        key = frameObj.getProperty('key')
+        self.removeForce(key, callFromFrameObj=True)
 
     def printForces(self):
         for key in self.externalForces.keys():
             print key
+
+    def saveForceLocationsToFile(self, filename=None, verbose=False):
+        if filename is None:
+            filename = "testDirector.csv"
+
+        drcBase = os.getenv('DRC_BASE')
+        fullFilePath = drcBase + "/software/control/residual_detector/src/" + filename
+        fileObject = open(fullFilePath, 'w')
+        for key, val in self.externalForces.iteritems():
+            line = str(val['linkName']) + ","
+
+            for i in range(0,3):
+                line += str(val['forceLocation'][i]) + ","
+
+            for i in range(0,3):
+                line += str(val['forceDirection'][i]) + ","
+
+            line += "\n"
+
+            if verbose:
+                print line
+
+            fileObject.write(line)
+
+        fileObject.close()
+
+    def addForcesFromFile(self, filename=None):
+        self.startCaptureMode()
+        if filename is None:
+            filename = "testDirector.csv"
+
+        drcBase = os.getenv('DRC_BASE')
+        fullFilePath = drcBase + "/software/control/residual_detector/src/" + filename
+        fileObject = open(fullFilePath, 'r')
+
+        reader = csv.reader(fileObject)
+        for row in reader:
+            line = []
+            for col in row:
+                line.append(col)
+
+            linkName = line[0]
+            forceLocation = np.array([float(line[1]), float(line[2]), float(line[3])])
+            forceDirection = np.array([float(line[4]), float(line[5]), float(line[6])])
+            self.addForce(linkName, wrench=None, forceDirection=forceDirection, forceMagnitude=0.0, forceLocation=forceLocation, inWorldFrame=False)
 
     # these are test methods
     def setupTest(self):
