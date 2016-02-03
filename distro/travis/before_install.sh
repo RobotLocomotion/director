0fdcce57cc39b3abbdbbadb7bfe11b6519fa9af5
Pat Marion <james.patrick.marion@gmail.com>
2016-02-03 12:28:00 -0500

    use sudo to install apt-get deps.  container does not have vtk whitelisted

--- distro/travis/before_install.sh
+++ distro/travis/before_install.sh
@@ -1,4 +1,9 @@
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+	sudo apt-get update -qq
+  sudo apt-get install libqt4-dev libvtk5-dev libvtk5-qt4-dev python-vtk python-numpy
+elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
 	brew update
   brew tap homebrew/science
   brew tap homebrew/python
