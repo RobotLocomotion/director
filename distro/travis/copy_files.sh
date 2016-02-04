98e09b01ef99160226aa604a81c92b9709cce058
Pat Marion <james.patrick.marion@gmail.com>
2016-02-03 20:23:25 -0500

    update copy_files.sh

--- distro/travis/copy_files.sh
+++ distro/travis/copy_files.sh
@@ -2,4 +2,4 @@
 
 scriptDir=$(cd $(dirname $0) && pwd)
 tar -zxf $scriptDir/test_key.enc -C /tmp
-scp -i /tmp/test_key -o StrictHostKeyChecking=no -r $*  pat@128.30.27.15:/tmp/
+scp -i /tmp/test_key -o StrictHostKeyChecking=no -r $*  travis@128.30.27.15:files/
