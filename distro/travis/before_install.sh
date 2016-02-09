643baa4425eaefc05c055afcd51d79e5e90229d9
Pat Marion <james.patrick.marion@gmail.com>
2016-02-09 18:24:53 -0500

    update ubuntu apt-get install command for travis-ci

--- distro/travis/before_install.sh
+++ distro/travis/before_install.sh
@@ -18,7 +18,7 @@ install_vtk_homebrew_bottle()
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	sudo apt-get update -qq
-  sudo apt-get install -y libqt4-dev libvtk5-dev libvtk5-qt4-dev python-vtk python-numpy
+  sudo apt-get install -y libqt4-dev libvtk5-dev libvtk5-qt4-dev libvtk-java python-dev python-vtk python-numpy
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
 	brew update
   brew tap homebrew/python
