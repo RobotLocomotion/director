afd7d7661a0ff0b70a7900c6e307a8a50c751921
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2016-05-23 18:34:46 -0400

    add utility to help publish polynomial message

--- src/python/director/lcmUtils.py
+++ src/python/director/lcmUtils.py
@@ -11,6 +11,8 @@ import imp
 import sys
 import re
 
+import drake
+
 class GlobalLCM(object):
 
   _handle = None
@@ -504,3 +506,25 @@ class LCMLoggerManager(object):
         for pid, command in self.existingLoggerProcesses.iteritems():
             processName, args = command
             os.system('kill %d' % pid)
+
+
+
+# Utilities for encoding piecewise polynomials
+def encodePolynomialVector(coefficientMatrix):
+    msg = drake.lcmt_polynomial_matrix()
+    msg.rows = coefficientMatrix.shape[0]
+    msg.cols = 1
+    numCoefficients = coefficientMatrix.shape[1]
+
+    polynomialMsgList = []
+
+    for rowIdx in xrange(0,msg.rows):
+        polynomialMsg = drake.lcmt_polynomial()
+        polynomialMsg.num_coefficients = numCoefficients
+        polynomialMsg.coefficients = coefficientMatrix[rowIdx,:]
+        polynomialMsgList.append([polynomialMsg])
+
+    msg.polynomials = polynomialMsgList
+
+    return msg
+
