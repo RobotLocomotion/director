beeed0dac4faa9d388592ae785f8536296596f32
Robin Deits <robin.deits@gmail.com>
2016-06-01 13:42:30 -0700

    update bottle to a version that thinks its from my tap

--- distro/travis/before_install.sh
+++ distro/travis/before_install.sh
@@ -15,7 +15,7 @@ make_vtk_homebrew_bottle()
 
 install_vtk_homebrew_bottle()
 {
-  wget https://dl.dropboxusercontent.com/s/mtacwrjgmtanfaz/vtk5-5.10.1_2.mavericks.bottle.1.tar.gz
+  wget https://www.dropbox.com/s/r0o7b3zrv6een6o/vtk5-5.10.1_2.mavericks.bottle.1.tar.gz?dl=0
   brew tap rdeits/director
   brew install vtk5-5.10.1_2.mavericks.bottle.1.tar.gz
 }
