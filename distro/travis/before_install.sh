d03b781ba4c084c697e45c23a8d411cbaffe37b6
Robin Deits <robin.deits@gmail.com>
2016-06-01 13:24:46 -0700

    switch to my tap for vtk5 since it's not in homebrew/science anymore

--- distro/travis/before_install.sh
+++ distro/travis/before_install.sh
@@ -7,7 +7,7 @@ scriptDir=$(cd $(dirname $0) && pwd)
 
 make_vtk_homebrew_bottle()
 {
-  brew tap homebrew/science
+  brew tap rdeits/director
   $scriptDir/brew_install.sh vtk5 --with-qt
   brew bottle vtk5
   $scriptDir/copy_files.sh vtk5*.tar.gz
@@ -16,7 +16,7 @@ make_vtk_homebrew_bottle()
 install_vtk_homebrew_bottle()
 {
   wget https://dl.dropboxusercontent.com/s/mtacwrjgmtanfaz/vtk5-5.10.1_2.mavericks.bottle.1.tar.gz
-  brew tap homebrew/science
+  brew tap rdeits/director
   brew install vtk5-5.10.1_2.mavericks.bottle.1.tar.gz
 }
 
