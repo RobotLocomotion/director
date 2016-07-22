96f1e3de3f8f44463ef4de2cf47db4bbd6abd350
Christian Rauch <Christian.Rauch@ed.ac.uk>
2016-07-22 16:13:24 +0100

    sequence of hand opening/closing actions with increasing closing amount

--- src/python/director/finger_test.py
+++ src/python/director/finger_test.py
@@ -1,8 +1,11 @@
 from director.tasks.taskuserpanel import TaskUserPanel
 from director import lcmUtils
+from director import objectmodel as om
+import director.tasks.robottasks as rt
 import bot_core
 import time
 import warnings
+from functools import partial
 
 def custom_formatwarning(msg, *a):
     # ignore everything except the message
@@ -10,6 +13,7 @@ def custom_formatwarning(msg, *a):
 
 warnings.simplefilter('always', UserWarning)
 warnings.formatwarning = custom_formatwarning
+
 
 # logic and communication
 class FingerTest(object):
@@ -23,10 +27,22 @@ class FingerTest(object):
                                   'ThumbFingerMotorRoll'        # 5
         ]
 
-        self.min = 0.0
-        self.max = 1.0
+        # never change these hard constraints
+        self.hard_min = 0.0
+        self.hard_max = 1.0
+
+        self.min = self.hard_min
+        self.max = 0.5
         self.hands = ['left', 'right']
         self.robot_name = ""
+
+    def setMin(self, val_min):
+        # set new min bound but check for hard constraints
+        self.min = max(val_min, self.hard_min)
+
+    def setMax(self, val_max):
+        # set new max bound but check for hard constraints
+        self.min = min(val_max, self.hard_max)
 
     # send desired values to all fingers, e.g. open and close fist
     def send(self, hand, angles):
@@ -67,10 +83,10 @@ class FingerTest(object):
             amount = min(amount, self.max)
         self.send(hand_name, [amount] * len(self.finger_joint_list))
 
-    def close_left(self, amount=0.5):
+    def close_left(self, amount):
         self.close("left", amount)
 
-    def close_right(self, amount=0.5):
+    def close_right(self, amount):
         self.close("right", amount)
 
     # send desired closure to individual finger
@@ -106,9 +122,68 @@ class FingerTestTaskPanel(TaskUserPanel):
         self.ft = fingertest
         TaskUserPanel.__init__(self, windowTitle='Finger test')
 
+        self.active_hand_label = "Active Hand"
+        self.min_label = "Min Amount"
+        self.max_label = "Max Amount"
+
+        self.active_hand = "left"
+
+        self.step = 0.1
+
+        self.addButtons()
+        self.addProperties()
+        self.addTasks()
+
+
+    def addButtons(self):
         self.addManualButton('Open Left Hand', self.ft.open_left)
         self.addManualButton('Open Right Hand', self.ft.open_right)
-
         self.addManualSpacer()
-        self.addManualButton('Close 50% Left Hand', self.ft.close_left)
-        self.addManualButton('Close 50% Right Hand', self.ft.close_right)
+        self.addManualButton('Open Active Hand', self.open)
+        self.addManualButton('Max Close Active Hand', self.close_max)
+        self.addManualSpacer()
+        self.addManualButton('New Task Sequence', self.addTasks)
+
+    def addProperties(self):
+        # active hand
+        self.params.addProperty(self.active_hand_label, 0, attributes=om.PropertyAttributes(enumNames=['Left', 'Right']))
+        # angle limits
+        self.params.addProperty(self.min_label, self.ft.min, attributes=om.PropertyAttributes(decimals=1, minimum=self.ft.hard_min, maximum=self.ft.hard_max, singleStep=0.1))
+        self.params.addProperty(self.max_label, self.ft.max, attributes=om.PropertyAttributes(decimals=1, minimum=self.ft.hard_min, maximum=self.ft.hard_max, singleStep=0.1))
+
+    def addTasks(self):
+        self.taskTree.removeAllTasks()
+        # tasks
+        openSequenceTask = self.taskTree.addGroup('Opening/Closing Hand Sequence')
+        self.taskTree.onAddTask(rt.CallbackTask(callback=self.open, name="Open"), parent=openSequenceTask, copy=False)
+
+        if(self.ft.min < self.step):
+            v = self.ft.min+self.step
+        else:
+            v = self.ft.min
+        while v<=self.ft.max:
+            self.taskTree.onAddTask(rt.CallbackTask(callback=partial(self.close, v), name="Close "+str(v)), parent=openSequenceTask, copy=False)
+            self.taskTree.onAddTask(rt.CallbackTask(callback=self.open, name="Open"), parent=openSequenceTask, copy=False)
+            v += self.step
+
+    def onPropertyChanged(self, propertySet, propertyName):
+        if propertyName == self.active_hand_label:
+            self.active_hand = self.params.getPropertyEnumValue(self.active_hand_label).lower()
+            self.appendMessage("Active hand: " + self.active_hand)
+
+        if propertyName == self.min_label:
+            self.ft.min = self.params.getProperty(self.min_label)
+
+        if propertyName == self.max_label:
+            self.ft.max = self.params.getProperty(self.max_label)
+
+    def open(self):
+        self.appendMessage("Opening "+self.active_hand+" hand")
+        self.ft.open(self.active_hand)
+
+    def close(self, amount):
+        self.appendMessage("Closing " + self.active_hand + " hand by " + str(amount))
+        self.ft.close(self.active_hand, amount)
+
+    def close_max(self):
+        self.close(self.ft.max)
