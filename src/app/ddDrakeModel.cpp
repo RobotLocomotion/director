4f1381f0806a5ae0d383389704a41b311552585f
Chien-Liang Fok <liang.fok@tri.global>
2016-12-03 23:10:46 -0500

    Updated URDFRigidBodyTreeVTK to use URDF parser directly rather than through RigidBodyTree.

--- src/app/ddDrakeModel.cpp
+++ src/app/ddDrakeModel.cpp
@@ -5,6 +5,8 @@
 #include <drake/systems/plants/RigidBodyTree.h>
 #include <drake/systems/plants/shapes/Geometry.h>
 #else
+#include <drake/multibody/joints/floating_base_types.h>
+#include <drake/multibody/parser_urdf.h>
 #include <drake/multibody/rigid_body_tree.h>
 #include <drake/multibody/shapes/geometry.h>
 #endif
@@ -737,7 +739,16 @@ public:
 URDFRigidBodyTreeVTK::Ptr loadVTKModelFromXML(const QString& xmlString, const QString& rootDir="")
 {
   URDFRigidBodyTreeVTK::Ptr model(new URDFRigidBodyTreeVTK);
+
+#ifdef DRAKE_OH_FORK
   model->addRobotFromURDFString(xmlString.toUtf8().constData(), PackageSearchPaths, rootDir.toLatin1().constData());
+#else
+  drake::parsers::urdf::AddModelInstanceFromUrdfStringSearchingInRosPackages(
+      xmlString.toUtf8().constData(), PackageSearchPaths,
+      rootDir.toLatin1().constData(), drake::multibody::joints::kRollPitchYaw,
+      nullptr /* weld to frame */, model.get());
+#endif
+
   model->computeDofMap();
   model->loadVisuals(rootDir);
   return model;
