7a3e39f3670c6db74f0eecf115a70cc7e21a9e6a
Chien-Liang Fok <liang.fok@tri.global>
2016-12-06 10:35:04 -0500

    Removed #ifdef DRAKE_OH_FORK logic from ddDrakeModel.cpp since this file is only compiled for drake/master.

--- src/app/ddDrakeModel.cpp
+++ src/app/ddDrakeModel.cpp
@@ -1,15 +1,10 @@
 #include "ddDrakeModel.h"
 #include "ddSharedPtr.h"
 
-#ifdef DRAKE_OH_FORK
-#include <drake/systems/plants/RigidBodyTree.h>
-#include <drake/systems/plants/shapes/Geometry.h>
-#else
 #include <drake/multibody/joints/floating_base_types.h>
 #include <drake/multibody/parser_urdf.h>
 #include <drake/multibody/rigid_body_tree.h>
 #include <drake/multibody/shapes/geometry.h>
-#endif
 
 #include <vtkPolyData.h>
 #include <vtkAppendPolyData.h>
@@ -740,14 +735,10 @@ URDFRigidBodyTreeVTK::Ptr loadVTKModelFromXML(const QString& xmlString, const QS
 {
   URDFRigidBodyTreeVTK::Ptr model(new URDFRigidBodyTreeVTK);
 
-#ifdef DRAKE_OH_FORK
-  model->addRobotFromURDFString(xmlString.toUtf8().constData(), PackageSearchPaths, rootDir.toLatin1().constData());
-#else
   drake::parsers::urdf::AddModelInstanceFromUrdfStringSearchingInRosPackages(
       xmlString.toUtf8().constData(), PackageSearchPaths,
       rootDir.toLatin1().constData(), drake::multibody::joints::kRollPitchYaw,
       nullptr /* weld to frame */, model.get());
-#endif
 
   model->computeDofMap();
   model->loadVisuals(rootDir);
