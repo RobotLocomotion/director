d55e1f7a7f150a6e8883e6ca811c2bcbc30e9db2
Robin Deits <robin.deits@gmail.com>
2016-12-18 23:25:18 -0500

    more attempts to get docker submission running

--- distro/docker/run_tests.sh
+++
@@ -1,2 +0,0 @@
-Xvfb :99 -ac -screen 0 1280x1024x16 &
-ctest -j 1 --dashboard Experimental --track travis --output-on-failure
--- distro/docker/run_tests_and_package.sh
+++ distro/docker/run_tests_and_package.sh
@@ -1,4 +1,7 @@
+set -xe
+
 Xvfb :99 -ac -screen 0 1280x1024x16 &
+ctest -j 1 --dashboard Experimental --track travis --output-on-failure
 cd /root/distro/package
 ./make_linux_package.sh 2>&1 > log.txt || cat log.txt
   /root/distro/travis/copy_files.sh /root/distro/package/*.tar.gz
