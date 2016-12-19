156602475201016113bf2ec5fa9dc5b4c674eb2d
Robin Deits <robin.deits@gmail.com>
2016-12-19 13:05:11 -0500

    add args to docker build

--- distro/docker/run_tests_and_package.sh
+++ distro/docker/run_tests_and_package.sh
@@ -1,6 +1,13 @@
 set -xe
 
+# We need to run Xvfb in the same shell script in which we 
+# will run ctest and the packaging script (which both use
+# directorPython. That's because if we run Xvfb from a 
+# separate docker RUN command, it will get killed before we
+# run the tests. 
 Xvfb :99 -ac -screen 0 1280x1024x16 &
+
+
 ctest -j 1 --dashboard Experimental --track travis --output-on-failure
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
