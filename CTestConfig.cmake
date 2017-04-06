e30ce653ef745ee7d2fd9f913248ab168149f34c
Pat Marion <james.patrick.marion@gmail.com>
2017-04-06 15:25:57 -0400

    change cdash site ip address

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,7 @@
-set(CTEST_PROJECT_NAME "DRC")
-set(CTEST_NIGHTLY_START_TIME "22:00:00 EDT")
+set(CTEST_PROJECT_NAME "director")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "kobol.csail.mit.edu")
-set(CTEST_DROP_LOCATION "/cd/submit.php?project=Director")
+set(CTEST_DROP_SITE "128.30.27.135")
+set(CTEST_DROP_LOCATION "/submit.php?project=director")
 set(CTEST_DROP_SITE_CDASH TRUE)
