56bc4de38826a49e880f1b80a01135190ff570ab
Robin Deits <robin.deits@gmail.com>
2017-04-26 18:36:45 -0400

    install breathe

--- distro/travis/before_install.sh
+++ distro/travis/before_install.sh
@@ -24,7 +24,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     libvtk-java python-dev python-vtk python-numpy python-yaml python-lxml xvfb \
     doxygen graphviz python-sphinx python-coverage
 
-  sudo pip install sphinx_rtd_theme
+  sudo pip install --upgrade sphinx_rtd_theme breathe
 
   python -c "import sphinx_rtd_theme"
 
