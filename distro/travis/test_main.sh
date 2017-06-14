345f1a880415ebbcea9243030635c43a5dd3c8a4
Pat Marion <james.patrick.marion@gmail.com>
2017-06-13 21:59:44 -0700

    update xvfb command in docker test script

--- distro/travis/test_main.sh
+++ distro/travis/test_main.sh
@@ -11,7 +11,8 @@ $scriptDir/install_deps.sh
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   # Xvfb needs to be running during testing and packaging
-  Xvfb :99 -ac -screen 0 1280x1024x16 &
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile \
+                          --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 fi
 
 $scriptDir/build_and_test.sh
