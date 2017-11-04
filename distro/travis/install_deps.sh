bf2ff8e3a01a65d2a30c782bd23755ab10b4dc56
Pat Marion <james.patrick.marion@gmail.com>
2017-11-04 10:35:22 -0700

    test fix for travis-ci

--- distro/travis/install_deps.sh
+++ distro/travis/install_deps.sh
@@ -56,17 +56,18 @@ install_ubuntu_deps()
 
 install_osx_deps()
 {
+  export HOMEBREW_NO_AUTO_UPDATE=1
+
   brew tap patmarion/director
   brew tap-pin patmarion/director
 
-  brew update > brew_update_log.txt
+  # don't update for now because we will used a fixed version
+  #brew update > brew_update_log.txt
 
   # checkout a fixed version of homebrew/core formula from August 12th 2017
-  export HOMEBREW_NO_AUTO_UPDATE=1
-  cd $(brew --repository homebrew/core) && git reset --hard 543d8e9b27b9bb25ce4491773684c30c8cb66dcc
-  #brew upgrade
+  cd $(brew --repository homebrew/core) && git fetch && git reset --hard 543d8e9b27b9bb25ce4491773684c30c8cb66dcc
 
-  brew install doxygen graphviz libyaml vtk7
+  brew install doxygen graphviz libyaml vtk7 || echo "error on brew install"
   brew install glib # for lcm
   brew ls --versions python || brew install python
   brew ls --versions numpy || brew install numpy || echo "error on brew install numpy"
