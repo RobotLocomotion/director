1c8435d592d7e4c612c8f53aa9b6d6a5d7f613bf
Pat Marion <james.patrick.marion@gmail.com>
2018-01-31 00:32:38 -0500

    add taskrunner.py and test

---
+++ src/python/director/taskrunner.py
@@ -0,0 +1,62 @@
+import sys
+import time
+import sys
+from threading import Thread
+
+from director import asynctaskqueue
+from director.timercallback import TimerCallback
+
+
+class TaskRunner(object):
+
+    def __init__(self):
+        self.interval = 1/60.0
+        sys.setcheckinterval(1000)
+        try:
+            sys.setswitchinterval(self.interval)
+        except AttributeError:
+            # sys.setswitchinterval is only python3
+            pass
+
+        self.taskQueue = asynctaskqueue.AsyncTaskQueue()
+        self.pendingTasks = []
+        self.threads = []
+        self.timer = TimerCallback(callback=self._onTimer, targetFps=1/self.interval)
+
+        # call timer.start here to initialize the QTimer now on the main thread
+        self.timer.start()
+
+    def _onTimer(self):
+
+        # add all tasks in self.pendingTasks to the AsyncTaskQueue
+        if self.pendingTasks:
+            while True:
+                try:
+                    self.taskQueue.addTask(self.pendingTasks.pop(0))
+                except IndexError:
+                    break
+
+            # start the AsyncTaskQueue if it's not already running
+            if self.taskQueue.tasks and not self.taskQueue.isRunning:
+                self.taskQueue.start()
+
+        # only retain the live threads
+        self.threads = [t for t in self.threads if t.is_alive()]
+
+        if self.threads:
+            # Give up control to other python threads that are running
+            time.sleep(self.interval)
+        else:
+            # return false to stop the timer
+            return False
+
+    def callOnMain(self, func, *args, **kwargs):
+        self.pendingTasks.append(lambda: func(*args, **kwargs))
+        self.timer.start()
+
+    def callOnThread(self, func, *args, **kwargs):
+        t = Thread(target=lambda: func(*args, **kwargs))
+        self.threads.append(t)
+        t.start()
+        self.timer.start()
+        return t
---
+++ src/python/tests/testTaskRunner.py
@@ -0,0 +1,20 @@
+from director import consoleapp
+from director.taskrunner import TaskRunner
+
+
+def testThread():
+  print 'on thread'
+  taskRunner.callOnMain(testMain)
+
+def testMain():
+  print 'on main'
+  app.quit()
+
+
+app = consoleapp.ConsoleApp()
+app.showPythonConsole()
+
+taskRunner = TaskRunner()
+taskRunner.callOnThread(testThread)
+
+app.start(enableAutomaticQuit=False)
