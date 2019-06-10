84d3a81c14807aafc3c788594d63e82231de4565
Pat Marion <james.patrick.marion@gmail.com>
2019-06-10 14:49:32 -0700

    update homebrew commands

--- distro/travis/install_deps.sh
+++ distro/travis/install_deps.sh
@@ -38,6 +38,7 @@ install_ubuntu_deps_common()
   fi
 }
 
+
 install_ubuntu_deps()
 {
   install_ubuntu_deps_common
@@ -53,27 +54,15 @@ install_ubuntu_deps()
   rm -rf /var/lib/apt/lists/*
 }
 
+
 install_osx_deps()
 {
-  export HOMEBREW_NO_AUTO_UPDATE=1
-
-  brew tap patmarion/director
-  brew tap-pin patmarion/director
-
-  # don't update for now because we will used a fixed version
-  #brew update > brew_update_log.txt
-
-  # checkout a fixed version of homebrew/core formula from August 12th 2017
-  cd $(brew --repository homebrew/core) && git fetch && git reset --hard 543d8e9b27b9bb25ce4491773684c30c8cb66dcc
-
-  brew install qt vtk7python3
-  brew install doxygen graphviz libyaml
-  brew install glib # for lcm
-  brew ls --versions python3 || brew install python3
-  #brew ls --versions numpy || brew install numpy --with-python3 || echo "error on brew install numpy"
-  pip3 install coverage lxml pyyaml Sphinx sphinx-rtd-theme numpy
+  brew update > brew_log.txt 2>&1 || cat brew_log.txt
+  brew cleanup > brew_log.txt 2>&1 || cat brew_log.txt
+  brew upgrade cmake qt python > brew_log.txt 2>&1 || cat brew_log.txt
+  brew install vtk
+  pip3 install coverage lxml PyYAML numpy scipy
 }
-
 
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
