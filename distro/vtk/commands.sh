1404d03d3decc6326790765da9a4aadafe10262f
Pat Marion <james.patrick.marion@gmail.com>
2019-06-21 20:45:27 -0400

    add vtk docker scripts

---
+++ distro/vtk/commands.sh
@@ -0,0 +1,9 @@
+docker build -f vtk.dockerfile -t vtk:tmp .
+
+
+docker run -it vtk:tmp bash -c "scp vtk*.tar.gz pmarion@patmarion.com:public_html/bottles/"
+
+
+#id=$(docker create vtk:tmp)
+#docker cp -a $id:/root/vtk7.1-qt5.9-python2.7-ubuntu18.04.tar.gz ./
+#docker rm -v $id
